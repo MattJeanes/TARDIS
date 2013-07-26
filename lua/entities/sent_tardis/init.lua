1150834c7ba85ffb32f82df064ea9ca93c90d6a9
Matt Jeanes <mattjeanes23@gmail.com>
2013-07-26 17:09:08 +0100

    Players can now enter their own tardis inside the interior...
    ... without it breaking.

--- lua/entities/sent_tardis/init.lua
+++ lua/entities/sent_tardis/init.lua
@@ -470,7 +470,7 @@ end
 function ENT:PlayerEnter( ply )
 	if self.occupants then
 		for k,v in pairs(self.occupants) do
-			if ply==v then return end
+			if ply==v and not ply.tardis_viewmode then return end
 		end
 	end
 	if ply.tardis and IsValid(ply.tardis) then
