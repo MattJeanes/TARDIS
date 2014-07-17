9e6aa1b65be76b7d72449cc11ecd382b6d5103e5
Matt Jeanes <mattjeanes23@gmail.com>
2014-07-17 19:05:59 +0100

    Adjusted portals and updated interior model

--- lua/entities/gmod_tardis_interior/modules/sh_portals.lua
+++ lua/entities/gmod_tardis_interior/modules/sh_portals.lua
@@ -4,12 +4,14 @@ if SERVER then
 		self.portals={}
 		for i=1,2 do
 			self.portals[i]=ents.Create("linked_portal_door")
-			self.portals[i]:SetWidth(45)
-			self.portals[i]:SetHeight(85)
 			self.portals[i]:SetDisappearDist(500)
 		end
-		self.portals[1]:SetPos(self.exterior:LocalToWorld(Vector(20,0,50)))
-		self.portals[2]:SetPos(self:LocalToWorld(Vector(0,-330,143)))
+		self.portals[1]:SetWidth(45)
+		self.portals[2]:SetWidth(40)
+		self.portals[1]:SetHeight(91)
+		self.portals[2]:SetHeight(80)
+		self.portals[1]:SetPos(self.exterior:LocalToWorld(Vector(20,0,48)))
+		self.portals[2]:SetPos(self:LocalToWorld(Vector(-1.5,-334,130.7)))
 		self.portals[1]:SetAngles(self.exterior:GetAngles())
 		self.portals[2]:SetAngles(self:LocalToWorldAngles(Angle(0,90,0)))
 		self.portals[1]:SetExit(self.portals[2])
