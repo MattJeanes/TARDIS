374d38dbcc67fd5e65e4678fe654fdba0a64f0fb
Matt Jeanes <mattjeanes23@gmail.com>
2015-04-30 00:16:00 +0100

    Hotfix again
    This file didn't go in the last commit for some reason:
    
    https://github.com/MattJeanes/TARDIS/commit/71efb42b5cc082ee33c13568b2e22eb0b87f3dba

--- lua/entities/gmod_tardis_interior/modules/cl_cordon.lua
+++ lua/entities/gmod_tardis_interior/modules/cl_cordon.lua
@@ -3,10 +3,13 @@
 hook.Add("wp-prerender", "tardisi-cordon", function(portal,exit,origin)
 	local parent=exit:GetParent()
 	if parent.TardisInterior then
-		for k,v in pairs(parent.props) do
+		for k,v in pairs(parent.cordon) do
 			if IsValid(k) then
 				k.olddraw=k:GetNoDraw()
 				k:SetNoDraw(false)
+				if k:IsPlayer() then
+					GAMEMODE:UpdateAnimation(k,k:GetVelocity(),0)
+				end
 			end
 		end
 	end
@@ -15,7 +18,7 @@ end)
 hook.Add("wp-postrender", "tardisi-cordon", function(portal,exit,origin)
 	local parent=exit:GetParent()
 	if parent.TardisInterior then
-		for k,v in pairs(parent.props) do
+		for k,v in pairs(parent.cordon) do
 			if IsValid(k) then
 				k:SetNoDraw(k.olddraw)
 				k.olddraw=nil
@@ -24,49 +27,62 @@ hook.Add("wp-postrender", "tardisi-cordon", function(portal,exit,origin)
 	end
 end)
 
+--[[
+local blendtemp=1
+hook.Add("PrePlayerDraw", "tardisi-cordon", function(ply)
+	blendtemp=render.GetBlend()
+	render.SetBlend(1)
+end)
+
+hook.Add("PostPlayerDraw", "tardisi-cordon", function(ply)
+	render.SetBlend(blendtemp)
+end)
+]]--
+
 ENT:AddHook("Initialize", "cordon", function(self)
-	self.props={}
-	self.propscan=0
+	self.cordon={}
+	self.cordonscan=0
 end)
 
 function ENT:UpdateCordon()
 	local mins,maxs=self:LocalToWorld(self:OBBMins()), self:LocalToWorld(self:OBBMaxs())
 	local intardis=LocalPlayer():GetTardisData("interior")==self
 	for k,v in pairs(ents.FindInBox(mins,maxs)) do
-		self.props[v]=1
-		if not v.tardis_cordon then
-			v.tardis_cordon=v:GetNoDraw()
-		end
-		if v:GetNoDraw()==intardis then
+		if IsValid(v) and not v:IsPlayer() and not v:GetParent():IsPlayer() then
+			if self.cordon[v]==nil then
+				v.tardis_cordon=v:GetNoDraw()
+			end
 			v:SetNoDraw(not intardis)
+			self.cordon[v]=1
 		end
 	end
-	for k,v in pairs(self.props) do
+	
+	for k,v in pairs(self.cordon) do
 		if IsValid(k) then
-			if v==true then -- left
+			if v==true then --left
 				k:SetNoDraw(k.tardis_cordon)
 				k.tardis_cordon=nil
-				self.props[k]=nil
+				self.cordon[k]=nil
 			elseif v==1 then
-				self.props[k]=true
+				self.cordon[k]=true
 			end
 		else
-			self.props[k]=nil
+			self.cordon[k]=nil
 		end
 	end
 end
 
 ENT:AddHook("Think", "cordon", function(self)
-	if CurTime()>self.propscan then
-		self.propscan=CurTime()+1
+	if CurTime()>self.cordonscan then
+		self.cordonscan=CurTime()+1
 		self:UpdateCordon()
 	end
 end)
 
-ENT:AddHook("PlayerEnter", "cordon", function(self)
-	self:UpdateCordon()
+ENT:AddHook("PlayerEnter", "cordon", function(self,ply)
+	if ply==LocalPlayer() then self:UpdateCordon() end
 end)
 
-ENT:AddHook("PlayerExit", "cordon", function(self)
-	self:UpdateCordon()
+ENT:AddHook("PlayerExit", "cordon", function(self,ply)
+	if ply==LocalPlayer() then self:UpdateCordon() end
 end)
