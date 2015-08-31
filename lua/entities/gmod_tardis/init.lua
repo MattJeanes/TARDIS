630c5c8f3c847131a44688f1dfa094441ab7ca67
Matt Jeanes <mattjeanes23@gmail.com>
2015-08-31 22:05:38 +0100

    Added missing doors popup
    Like WAC, a popup will appear if you don't install Doors prompting you
    to either install it or uninstall this addon.

--- lua/entities/gmod_tardis/init.lua
+++ lua/entities/gmod_tardis/init.lua
@@ -5,3 +5,13 @@ include('shared.lua')
 ENT:AddHook("PostPlayerInitialize", "senddata", function(self,ply)
 	self:SendData(ply)
 end)
+
+if not TARDIS.DoorsFound then
+	function ENT:SpawnFunction(ply,...)
+		if not scripted_ents.GetStored(self.Base) then
+			ply:ChatPrint("Doors is not installed!")
+			ply:SendLua('TARDIS:ShowDoorsPopup()')
+			return
+		end
+	end
+end
---
+++ lua/tardis/sh_doors.lua
@@ -0,0 +1,37 @@
+local found=false
+local f=file.Find('entities/gmod_door_exterior/*.lua', "LUA")
+for k,v in pairs(f) do
+	if (SERVER and v=="init.lua") or (CLIENT and v=="cl_init.lua") then
+		found=true
+	end
+end
+TARDIS.DoorsFound=found
+
+if CLIENT then
+	function TARDIS:ShowDoorsPopup()
+		local DoorsFrame=vgui.Create('DFrame')
+		DoorsFrame:SetTitle("Doors is not installed")
+		DoorsFrame:SetSize(ScrW()*0.95, ScrH()*0.95)
+		DoorsFrame:SetPos((ScrW() - DoorsFrame:GetWide()) / 2, (ScrH() - DoorsFrame:GetTall()) / 2)
+		DoorsFrame:MakePopup()
+		
+		local h=vgui.Create('DHTML')
+		h:SetParent(DoorsFrame)
+		h:SetPos(DoorsFrame:GetWide()*0.005, DoorsFrame:GetTall()*0.03)
+		local x,y = DoorsFrame:GetSize()
+		h:SetSize(x*0.99,y*0.96)
+		h:SetAllowLua(true)
+		h:OpenURL('http://mattjeanes.com/abyss/doors-warning.html')
+		TARDIS.DoorsFrame=DoorsFrame
+	end
+end
+
+timer.Simple(5,function()
+	if not found and not TARDIS.DoorsFrame then
+		if CLIENT then
+			TARDIS:ShowDoorsPopup()
+		elseif SERVER then
+			timer.Create("Doors-NotInstalled", 10, 0, function() print("Doors is not installed!") end)
+		end
+	end
+end)
