71771d0b3855efec44e5364129608a6462ce8063
Win <Win7yes@users.noreply.github.com>
2019-01-13 02:45:09 -0300

    For the sake of not breaking things Switched from saving on top of Legacy locations to new location Apparently the vON lib in Rewrite serializes differently than the one on Legacy, yet is backwards compatible on read. Due to this I didn't notice that it would break Legacy if it tried to read it. Apologies.

--- lua/tardis/cl_savedlocations.lua
+++ lua/tardis/cl_savedlocations.lua
@@ -1,14 +1,20 @@
 TARDIS.Locations = TARDIS.Locations or {}
 
+local filename = "tardis_locations_nw.txt"
 
 function TARDIS:LoadLocations()
-    if file.Exists("tardis_locations.txt", "DATA") then
+    if file.Exists(filename,"DATA") then
+        TARDIS.Locations = TARDIS.von.deserialize(file.Read(filename,"DATA"))
+    elseif file.Exists("tardis_locations.txt", "DATA") then
         TARDIS.Locations = TARDIS.von.deserialize(file.Read("tardis_locations.txt", "DATA"))
+        if not file.Exists(filename,"DATA") then
+            TARDIS:SaveLocations()
+        end
     end
 end
 
 function TARDIS:SaveLocations()
-    file.Write("tardis_locations.txt", TARDIS.von.serialize(TARDIS.Locations))
+    file.Write(filename, TARDIS.von.serialize(TARDIS.Locations))
 end
 
 function TARDIS:AddLocation(pos,ang,name,map)
