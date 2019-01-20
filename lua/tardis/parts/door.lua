723abae594662d23a6546fdbe4faac598690c6db
Matt Jeanes <mattjeanes23@gmail.com>
2019-01-20 01:41:39 +0000

    It was annoying me.. the inconsistency!!

--- lua/tardis/parts/door.lua
+++ lua/tardis/parts/door.lua
@@ -39,8 +39,8 @@ if SERVER then
 	function PART:Use(a)
 		if self.exterior:GetData("locked") then
 			if IsValid(a) and a:IsPlayer() then
-				self:EmitSound("doors/door_lock_1.wav")
 				a:ChatPrint("The doors are locked.")
+				self:EmitSound("doors/door_lock_1.wav")
 			end
 		else
 			if a:KeyDown(IN_WALK) then
