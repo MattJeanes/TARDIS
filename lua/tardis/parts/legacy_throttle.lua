fde4abe82828bbe226b5b65b30f9f195928e87a2
Win <Win7yes@users.noreply.github.com>
2019-03-21 17:29:52 -0300

    whoops

--- lua/tardis/parts/legacy_throttle.lua
+++ lua/tardis/parts/legacy_throttle.lua
@@ -12,14 +12,13 @@ if SERVER then
     function PART:Use(ply)
         self:EmitSound("tardis/control_throttle.wav")
         if self.InSequence==true then return end
+        if self.exterior:GetData("teleport")==true or self.exterior:GetData("vortex")==true then
+            self.exterior:Mat()
+        end
         if self.interior:GetData("legacy-advanced",false)==true then return end
         if self.exterior:GetData("teleport",false)==false or self.exterior:GetData("vortex",false)==false then
             self.exterior:Demat()
         end 
-        
-        if self.exterior:GetData("teleport")==true or self.exterior:GetData("vortex")==true then
-            self.exterior:Mat()
-        end
     end
 end
         
