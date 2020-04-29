ec0ed458f0c6d9c314b028b45b8b0c89c9b3f654
Win <Win7yes@users.noreply.github.com>
2020-04-29 19:39:49 -0300

    whoops IsPlayer() to ply:IsPlayer()

--- lua/tardis/parts/legacy/legacy_typewriter.lua
+++ lua/tardis/parts/legacy/legacy_typewriter.lua
@@ -10,7 +10,7 @@ PART.Animate = true
 
 if SERVER then
     function PART:Use(ply)
-        if not IsPlayer(ply) then return end
+        if ply:IsPlayer() then return end
         ply:SendLua([[
             TARDIS:HUDScreen()
             TARDIS:SwitchScreen(TARDIS.screenpop, TARDIS:GetScreenByName("Destination"))
