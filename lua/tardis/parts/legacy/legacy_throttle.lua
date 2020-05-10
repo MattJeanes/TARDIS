dbaa4f8592ca06e29c8c0ef1499ef34df807c1cd
Win <Win7yes@users.noreply.github.com>
2020-05-10 20:33:45 -0300

    Remove redundant advanced mode switch

--- lua/tardis/parts/legacy/legacy_throttle.lua
+++ lua/tardis/parts/legacy/legacy_throttle.lua
@@ -15,7 +15,7 @@ if SERVER then
         if self.exterior:GetData("teleport")==true or self.exterior:GetData("vortex")==true then
             self.exterior:Mat()
         end
-        if self.interior:GetData("legacy-advanced",false)==true then return end
+        if TARDIS:GetSetting("csequences-enabled",false,self:GetCreator()) then return end
         if self.exterior:GetData("teleport",false)==false or self.exterior:GetData("vortex",false)==false then
             self.exterior:Demat()
         end 
