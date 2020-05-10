f7a32c849072d6326145376f5a9b75302916beab
Win <Win7yes@users.noreply.github.com>
2020-05-10 20:32:18 -0300

    Update sh_doorcontroller.lua Fixes improper reporting of door state

--- lua/tardis/screens/sh_doorcontroller.lua
+++ lua/tardis/screens/sh_doorcontroller.lua
@@ -18,10 +18,11 @@ TARDIS:AddScreen("Door controller", {intonly=true,menu=false}, function(self,ext
 		doorstatus:SizeToContents()
 		doorstatus:SetPos((frame:GetWide()*0.5)-(doorstatus:GetWide()*0.5),(frame:GetTall()*0.35)-(doorstatus:GetTall()*0.5))
 	end
-	doorstatus:DoLayout()
+	doorstatus:DoLayout()	
 
 	local button = vgui.Create("DButton",frame)
 	button.first=true
+	button.moving = false
 	button:SetFont("TARDIS-Default")
 	button:SetSize(frame:GetWide()*0.3,frame:GetTall()*0.15)
 	button:SetPos((frame:GetWide()*0.5)-(button:GetWide()*0.5),(frame:GetTall()*0.6)-(button:GetTall()*0.5))
@@ -30,6 +31,8 @@ TARDIS:AddScreen("Door controller", {intonly=true,menu=false}, function(self,ext
 	end
 	button.Think = function(button)
 		if ext:DoorMoving() then
+			button.moving = true
+			button.first = true
 			if not button:GetDisabled() then
 				if button.open then
 					doorstatus:SetText("The door is closing")
@@ -40,9 +43,14 @@ TARDIS:AddScreen("Door controller", {intonly=true,menu=false}, function(self,ext
 				button:SetDisabled(true)
 			end
 		elseif button:GetDisabled() then
+			if not button.open then
+				button.moving = false
+			end
 			button:SetDisabled(false)
 		elseif ext:DoorOpen() and (not button.open) or button.first then
-			button.open=true
+			if not button.moving then
+				button.open = true
+			end
 			button:SetText("Close the door")
 			doorstatus:SetText("The door is open")
 			doorstatus:DoLayout()
@@ -58,5 +66,17 @@ TARDIS:AddScreen("Door controller", {intonly=true,menu=false}, function(self,ext
 				button.first=nil
 			end
 		end
+	end
+
+	local debuglabel = vgui.Create("DLabel",frame)
+	debuglabel:SetTextColor(Color(0,0,0))
+	debuglabel:SetFont("TARDIS-Default")
+	debuglabel.DoLayout = function(self)
+		debuglabel:SizeToContents()
+		debuglabel:SetPos((frame:GetWide()*0.5)-(debuglabel:GetWide()*0.5),(frame:GetTall()*0.5)-(debuglabel:GetTall()*0.5))
+	end
+	debuglabel.Think = function(debuglabel)
+		debuglabel:SetText( "open: "..tostring(ext:DoorOpen()).." button.open: "..tostring(button.open).." moving: "..tostring(ext:DoorMoving()) )
+		debuglabel:DoLayout()
 	end
 end)
