136183b39b33c7d9be602efdcab4fa37d65a406a
Win <Win7yes@users.noreply.github.com>
2020-05-12 13:36:38 -0300

    Gramaphone opens music panel

--- lua/tardis/parts/legacy/legacy_gramophone.lua
+++ lua/tardis/parts/legacy/legacy_gramophone.lua
@@ -5,6 +5,13 @@ PART.ID = "legacy_gramophone"
 PART.Name = "Legacy Gramophone"
 PART.Model = "models/drmatt/tardis/gramophone.mdl"
 PART.AutoSetup = true
-PART.Collision = true  
+PART.Collision = true
+
+if SERVER then
+    function PART:Use(ply)
+        if not ply:IsPlayer() then return end
+        TARDIS:PopToScreen("Music",ply)
+    end
+end
 
 TARDIS:AddPart(PART)
