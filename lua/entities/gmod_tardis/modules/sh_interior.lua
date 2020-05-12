01ebd9003e7b2426153cc0cc9214f2bd8ee09d5c
Dodo <46424259+Dodobro@users.noreply.github.com>
2020-05-12 10:22:16 +0100

    Fixed Alt + E into TARDIS
    Players can no longer Alt + E into a landing or taking off TARDIS.

--- lua/entities/gmod_tardis/modules/sh_interior.lua
+++ lua/entities/gmod_tardis/modules/sh_interior.lua
@@ -2,10 +2,12 @@
 
 if SERVER then
 	ENT:AddHook("Use", "interior", function(self,a,c)
-		if a:KeyDown(IN_WALK) or not IsValid(self.interior) then
-			self:PlayerEnter(a)
-		else
-			self:ToggleDoor()
+		if not self:GetData("teleport") then
+			if a:KeyDown(IN_WALK) or not IsValid(self.interior) then
+				self:PlayerEnter(a)
+			else
+				self:ToggleDoor()
+			end
 		end
 	end)
 end
