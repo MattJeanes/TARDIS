ae8c6b4a0b456022e4bdc972519ab87b45b1ddd6
Matt Jeanes <mattjeanes23@gmail.com>
2020-05-12 11:19:12 +0100

    Update sh_interior.lua

--- lua/entities/gmod_tardis/modules/sh_interior.lua
+++ lua/entities/gmod_tardis/modules/sh_interior.lua
@@ -2,12 +2,12 @@
 
 if SERVER then
 	ENT:AddHook("Use", "interior", function(self,a,c)
-		if not self:GetData("teleport") then
-			if a:KeyDown(IN_WALK) or not IsValid(self.interior) then
+		if a:KeyDown(IN_WALK) or not IsValid(self.interior) then
+			if self:CallHook("CanPlayerEnterDoor")~=false then
 				self:PlayerEnter(a)
-			else
-				self:ToggleDoor()
 			end
+		else
+			self:ToggleDoor()
 		end
 	end)
 end
