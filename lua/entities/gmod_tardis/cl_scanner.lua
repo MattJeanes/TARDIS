b580a1c8ed951be80432b1f5814445e56b1f318f
Win <Win7yes@users.noreply.github.com>
2020-05-12 16:51:20 -0300

    Check if interior exists otherwise we'll die

--- lua/entities/gmod_tardis/cl_scanner.lua
+++ lua/entities/gmod_tardis/cl_scanner.lua
@@ -1,5 +1,5 @@
 ENT:AddHook("VortexEnabled", "scanner", function(self)
-    if self.interior.scannerrender then
+    if self.interior and self.interior.scannerrender then
         return false
     end
 end)
