c2d4c0023d6ae60d3648fbfacd2541a7f1bb41b9
Win7yes <francodmeneguzzi@gmail.com>
2020-11-18 18:31:44 -0300

    Attempt to scale the health hud I don't have a smaller screen I can't test this properly

--- lua/tardis/cl_hud.lua
+++ lua/tardis/cl_hud.lua
@@ -5,10 +5,13 @@ hook.Add("HUDPaint", "TARDISRewrite-HUD", function()
     if not (LocalPlayer():GetTardisData("interior") or LocalPlayer():GetTardisData("exterior")) then return end
     local tardis = LocalPlayer():GetTardisData("exterior")
     if not IsValid(tardis) then return end
+    local sw = ScrW()
+    local sh = ScrH()
     local health = tardis:GetHealthPercent()
     local minwidth = 160
     local width = minwidth + math.Clamp( ((#tostring(health)-4) * 30), 0, math.huge)
-    local height = 120
+    local height = (sw >= 800) and 120 or 95
+    local healthfont = (height == 120) and "TARDIS-Large" or "TARDIS-Med"
     local x = (ScrW()-width)*0.02
     local y = (ScrH()-height)*0.025
     draw.RoundedBox( 10, x, y, width, height, NamedColor("BgColor") )
@@ -17,5 +20,5 @@ hook.Add("HUDPaint", "TARDISRewrite-HUD", function()
     if (health > 20) then textcolor = NamedColor("FgColor")
     else textcolor = NamedColor("Caution") end
     draw.DrawText( "TARDIS", "TARDIS-PageName", x+10, y+10, textcolor, TEXT_ALIGN_LEFT )
-    draw.DrawText( health, "TARDIS-Large", x+10, y+45, textcolor, TEXT_ALIGN_LEFT )
+    draw.DrawText( health, healthfont, x+10, y+45, textcolor, TEXT_ALIGN_LEFT )
 end)
