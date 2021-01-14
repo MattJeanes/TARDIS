6fd60279c9826581c81c7a7b0e9cad8e0738c5de
Win7yes <francodmeneguzzi@gmail.com>
2021-01-13 22:38:13 -0300

    Rewrite projected light system

--- lua/entities/gmod_tardis/modules/cl_projectedlight.lua
+++ lua/entities/gmod_tardis/modules/cl_projectedlight.lua
@@ -1,4 +1,5 @@
-
+--Fake light bleeding
+--Settings
 TARDIS:AddSetting({
 	id="extprojlight-enabled",
 	name="Door Light Enabled",
@@ -7,68 +8,73 @@ TARDIS:AddSetting({
 	value=true,
 	type="bool",
 	option=true,
-	networked=true
 })
-
-function ENT:CalcLightBrightness(interior)
-    local pos = (interior and self.interior:GetPos() or self:GetPos())
-    local lightcolour = render.GetLightColor(pos):ToColor()
-    local rm = 0.299*lightcolour.r
-    local gm = 0.587*lightcolour.g
-    local bm = 0.114*lightcolour.b
-    local luminance = rm + gm + rm
-    return luminance
+--Methods
+function ENT:CalcLightBrightness(pos)
+	local lightcolour = render.GetLightColor(pos):ToColor()
+	local rm = 0.299*lightcolour.r
+	local gm = 0.587*lightcolour.g
+	local bm = 0.114*lightcolour.b
+	local luminance = rm + gm + rm
+	return luminance
 end
 
-ENT:AddHook("Initialize", "projectedlight", function(self)
-    if not self.interior then return end
-    self.projlightsettings = {}
-    self.projectedlight = ProjectedTexture()
-    self.projectedlight:SetTexture("effects/flashlight/square")
-    self.projectedlight:SetFarZ(250)
-    self.projectedlight:SetVerticalFOV(self.metadata.Exterior.Portal.height)
-    self.projectedlight:SetHorizontalFOV(self.metadata.Exterior.Portal.width+10)
-    self.projectedlight:SetColor(self.metadata.Interior.Light.color)
-    --self.projectedlight:SetBrightness(0.5)
-	self.projectedlight:SetPos(self:LocalToWorld(Vector(0,0,50)))
-    self.projectedlight:SetAngles(self:GetAngles())
-    self.projlightsettings.basebrightness = 0.5
-    self.projlightsettings.calculatedbrightness = self.projlightsettings.basebrightness
-	self.projectedlight:Update()
-end)
+function ENT:CreateProjectedLight()
+	if self.projectedlight then return end
+	local pl = ProjectedTexture()
+	pl:SetTexture("effects/flashlight/square")
+	pl:SetFarZ(250)
+	pl:SetVerticalFOV(self.metadata.Exterior.Portal.height)
+	pl:SetHorizontalFOV(self.metadata.Exterior.Portal.width+10)
+	pl:SetColor(self.metadata.Interior.Light.color)
+    pl:SetBrightness(0.5)
+    pl:SetPos(self:LocalToWorld(Vector(-21,0,51.1)))
+    pl:SetEnableShadows(true)
+	pl:SetAngles(self:GetAngles())
+	pl:Update()
+	self.projectedlight = pl
+end
 
+function ENT:RemoveProjectedLight()
+	if self.projectedlight then
+		self.projectedlight:Remove()
+		self.projectedlight = nil
+	end
+end
+
+function ENT:UpdateProjectedLight()
+    self.projectedlight:SetPos(self:LocalToWorld(Vector(-21,0,51.1)))
+    self.projectedlight:SetAngles(self:GetAngles())
+    self.projectedlight:Update()
+end
+--Hooks
 ENT:AddHook("OnRemove", "projectedlight", function(self)
 	if ( IsValid( self.projectedlight ) ) then
 		self.projectedlight:Remove()
 	end
 end)
 
-ENT:AddHook("ShouldDrawProjectedLight", "setting", function(self)
-    if (not TARDIS:GetSetting("extprojlight-enabled")) then return false end
+ENT:AddHook("ShouldDrawProjectedLight", "baseShouldDraw", function(self)
+	return self:DoorOpen(true)
+end)
+
+ENT:AddHook("ShouldNotDrawProjectedLight","baseShouldntDraw",function(self)
+    if (not TARDIS:GetSetting("extprojlight-enabled")) or (not self.interior) then return true end
+    if self:GetData("vortex",false)==true then return true end
 end)
 
 ENT:AddHook("Think", "projectedlight", function(self)
-    if not self.interior then return end
-    if (self:CallHook("ShouldDrawProjectedLight")==false) then
+	if not self.interior then return end
+	local shouldon = self:CallHook("ShouldDrawProjectedLight")
+	local shouldoff = self:CallHook("ShouldNotDrawProjectedLight")
 
-        if self.projectedlight:GetEnableShadows() == true then 
-            self.projectedlight:SetEnableShadows(false)
-            self.projectedlight:SetBrightness(0)
-            self.projectedlight:Update()
+	if shouldon and (not shouldoff) then
+        if (not self.projectedlight) or (not IsValid(self.projectedlight)) then
+            self:CreateProjectedLight()
+        else
+            self:UpdateProjectedLight()
         end
-        return
+    elseif self.projectedlight then
+        self:RemoveProjectedLight()
     end
-    if self.projectedlight:GetBrightness() > 0 and not self:DoorOpen(true) then
-        self.projectedlight:SetEnableShadows(false)
-        self.projectedlight:SetBrightness(0)
-        self.projectedlight:Update()
-    elseif self:DoorOpen(true) and self.projectedlight:GetBrightness() == 0 then
-        self.projectedlight:SetEnableShadows(true)
-        self.projectedlight:SetBrightness(self.projlightsettings.calculatedbrightness)
-    end
-    if ( IsValid( self.projectedlight ) ) and self:DoorOpen(true) then
-		self.projectedlight:SetPos( self:LocalToWorld(Vector(-25,0,51.1)) )
-		self.projectedlight:SetAngles( self:LocalToWorldAngles(Angle(0,0,0)) )
-		self.projectedlight:Update()
-	end
 end)
