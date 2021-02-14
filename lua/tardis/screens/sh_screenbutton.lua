7cf942090cd62f71525018d89e4f531f854f457b
Artiom Parygin <artiom.par@ya.ru>
2021-02-14 19:46:11 +0300

    TardisScreenButton type for buttons with images; 2 test visual buttons
    A new GUI screen with a background and 2 buttons: door controller and
    a fast return button. The buttons have a special type, since
    DImageButton doesn't work in the 3D screen.

---
+++ lua/tardis/screens/sh_screenbutton.lua
@@ -0,0 +1,78 @@
+TardisScreenButton = {}
+
+function TardisScreenButton:new(parent)
+	local screen_button = {}
+
+	screen_button.icon=vgui.Create("DImageButton", parent)
+	screen_button.label=vgui.Create("DLabel", parent)
+	screen_button.label:SetColor(Color(255,255,255,0))
+	screen_button.is_toggle=false
+	screen_button.icon_off="materials/vgui/buttons/default_off.png"
+	screen_button.icon_on="materials/vgui/buttons/default_on.png"
+	screen_button.icon:SetImage(screen_button.icon_off)
+	screen_button.Think = function() end
+
+	function screen_button.icon:Think()
+		if not screen_button.is_toggle and screen_button.on
+			and CurTime() > screen_button.click_end_time
+		then
+			screen_button.icon:SetImage(screen_button.icon_off)
+			screen_button.on = false
+		end
+		screen_button.Think()
+	end
+
+
+	screen_button.icon.DoClick = function()
+		screen_button.DoClick()
+		screen_button.on = not screen_button.on
+		if screen_button.on then
+			screen_button.icon:SetImage(screen_button.icon_on)
+		else
+			screen_button.icon:SetImage(screen_button.icon_off)
+		end
+		screen_button.click_end_time = CurTime() + 1
+	end
+	screen_button.label.DoClick = screen_button.icon.DoClick
+
+	setmetatable(screen_button,self)
+	self.__index = self
+	return screen_button
+end
+
+function TardisScreenButton:SetSize(sizeX, sizeY)
+	self.icon:SetSize(sizeX, sizeY)
+	self.label:SetSize(sizeX, sizeY)
+end
+
+function TardisScreenButton:SetPos(posX, posY)
+	self.icon:SetPos(posX, posY)
+	self.label:SetPos(posX, posY)
+end
+
+function TardisScreenButton:SetImages(off, on)
+	self.icon_off = off
+	self.icon_on = on or off
+	self.icon:SetImage(self.icon_off)
+end
+
+function TardisScreenButton:SetIsToggle(is_toggle)
+	self.is_toggle=is_toggle
+end
+
+function TardisScreenButton:GetTall()
+	return self.icon:GetTall()
+end
+
+function TardisScreenButton:GetWide()
+	return self.icon:GetWide()
+end
+
+function TardisScreenButton:SetToggle(on)
+	self.on = on
+	if on then
+		self.icon:SetImage(self.icon_on)
+	else
+		self.icon:SetImage(self.icon_off)
+	end
+end
