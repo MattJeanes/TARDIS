0cee4a6ac170a96b4f894d6d3f82bca0f16c37d9
Win7yes <francodmeneguzzi@gmail.com>
2021-02-22 16:21:40 -0300

    Add sv_wiremod.lua to interior libraries

---
+++ lua/entities/gmod_tardis_interior/modules/libraries/sv_wiremod.lua
@@ -0,0 +1,61 @@
+--Wiremod support
+
+--Vars
+
+local inputs = inputs or {}
+local outputs = outputs or {}
+
+--Methods
+
+function ENT:AddWireInput(name, desc, type)
+	if not WireLib then return end
+	local input = {
+		name = name,
+		desc = desc,
+		datatype = type
+	}
+	table.insert(inputs, input)
+end
+
+function ENT:AddWireOutput(name, desc, type)
+	if not WireLib then return end
+	local output = {
+		name = name,
+		desc = desc,
+		datatype = type
+	}
+	table.insert(outputs, output)
+end
+
+--Hooks
+function ENT:TriggerInput(name, value)
+	if not WireLib then return end
+	self:CallHook("OnWireInput", name, value)
+end
+
+function ENT:TriggerWireOutput(name, value)
+	if not WireLib then return end
+	WireLib.TriggerOutput(self, name, value)
+end
+
+ENT:AddHook("Initialize", "wiremod", function(self)
+	if not WireLib then return end
+	local ins = {}
+	local indescs = {}
+	local intypes = {}
+	for k,v in ipairs(inputs) do
+		ins[k] = v.name
+		indescs[k] = v.desc or nil
+		intypes[k] = v.datatype or nil
+	end
+	WireLib.CreateSpecialInputs(self, ins, intypes, indescs)
+	local outs = {}
+	local outdescs = {}
+	local outtypes = {}
+	for k,v in ipairs(outputs) do
+		outs[k] = v.name
+		outdescs[k] = v.desc or nil
+		outtypes[k] = v.datatype or nil
+	end
+	WireLib.CreateSpecialOutputs(self, outs, outtypes, outdescs)
+end)
