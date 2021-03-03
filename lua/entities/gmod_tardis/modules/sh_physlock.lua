ac7d91a795d5057d52683ff1d15b1131aecfa3ad
Artiom Parygin <artiom.par@ya.ru>
2021-03-03 18:59:14 +0300

    Renamed physlock file to be shared and fixed legacy message

--- lua/entities/gmod_tardis/modules/sh_physlock.lua
+++ lua/entities/gmod_tardis/modules/sh_physlock.lua
@@ -84,3 +84,5 @@ if SERVER then
 		end
 	end)
 end
+
+
--- lua/tardis/parts/legacy/legacy_physbrake.lua
+++ lua/tardis/parts/legacy/legacy_physbrake.lua
@@ -11,8 +11,8 @@ PART.Sound = "tardis/control_handbrake.wav"
 
 if SERVER then
 	function PART:Use(ply)
-		local result = self.exterior:TogglePhyslock() or false
-		ply:ChatPrint("Physics Lock ".. (result and "engaged" or "disengaged"))
+		self.exterior:TogglePhyslock()
+		ply:ChatPrint("Physics Lock ".. (self.exterior:GetData("physlock") and "engaged" or "disengaged"))
 	end
 end
 
