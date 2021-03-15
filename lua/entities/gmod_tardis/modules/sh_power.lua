d019f4c6edb299e0aefe51586121cdbdc324b70f
Win7yes <francodmeneguzzi@gmail.com>
2021-03-15 18:01:39 -0300

    Power E2 functions

--- lua/entities/gmod_tardis/modules/sh_power.lua
+++ lua/entities/gmod_tardis/modules/sh_power.lua
@@ -10,19 +10,28 @@ end
 
 if SERVER then
 	function ENT:TogglePower()
-		self:SetPower(not self:GetPower())
+		return self:SetPower(not self:GetPower())
 	end
 	function ENT:SetPower(on)
 		if (self:CallHook("CanTogglePower")==false or self.interior:CallHook("CanTogglePower")==false) then return end
-		self:SetData("power-state",on,true)
+		local success = self:SetData("power-state",on,true)
 		self:CallHook("PowerToggled",on)
 		if self.interior then
 			self.interior:CallHook("PowerToggled",on)
 		end
+		return success
 	end
 
 	ENT:AddHook("CanTriggerHads","power",function(self)
 		if not self:GetPower() then return false end
+	end)
+
+	ENT:AddHook("HandleE2", "power", function(self,name,e2)
+		if name == "Power" then
+			return self:TogglePower()
+		elseif name == "GetPowered" then
+			return self:GetPower() and 1 or 0
+		end
 	end)
 else
 	ENT:AddHook("ShouldNotDrawProjectedLight", "power", function(self)
