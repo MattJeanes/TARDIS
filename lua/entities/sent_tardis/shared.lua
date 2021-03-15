0304a5e035a6b1552a8626d0c866eeab87bae7da
Artiom Parygin <artiom.par@ya.ru>
2021-03-15 15:14:46 +0300

    Apply suggestions from code review
    Co-authored-by: Matt Jeanes <mattjeanes23@gmail.com>

--- lua/entities/sent_tardis/shared.lua
+++ lua/entities/sent_tardis/shared.lua
@@ -4,7 +4,7 @@ if WireLib then
 else
 	ENT.Base			= "base_gmodentity"
 end 
-ENT.PrintName		= "Legacy TARDIS"
+ENT.PrintName		= " TARDIS Legacy "
 ENT.Author			= "Dr. Matt"
 ENT.Contact			= "mattjeanes23@gmail.com"
 ENT.Instructions	= "Use with the sonic or press E to pilot."
@@ -12,7 +12,7 @@ ENT.Purpose			= "Time and Relative Dimension in Space"
 ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 ENT.RenderGroup = RENDERGROUP_BOTH
-ENT.Category		= "Doctor Who - TARDIS (Legacy)"
+ENT.Category		= "Doctor Who - TARDIS"
 
 CreateConVar("tardis_takedamage", "1", {FCVAR_NOTIFY, FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED})
 CreateConVar("tardis_flightphase", "1", {FCVAR_NOTIFY, FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED})
