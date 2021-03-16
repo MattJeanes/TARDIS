a9d57697cb408f4eba2b77c285b202e8246c9cf4
Matt Jeanes <matthew.jeanes@myairops.com>
2021-03-16 16:02:23 +0000

    Improved pointer and respect spawning rules

--- lua/entities/gmod_tardis_debug_pointer/init.lua
+++ lua/entities/gmod_tardis_debug_pointer/init.lua
@@ -10,15 +10,15 @@ function ENT:Initialize()
  
 	self:SetModel( "models/brundoob/precision.mdl" )
 	self:PhysicsInit( MOVETYPE_VPHYSICS )
-	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetMoveType( MOVETYPE_NONE )
 	self:SetSolid( SOLID_VPHYSICS )
 	self:SetCollisionGroup( COLLISION_GROUP_WORLD )
+	self:SetUnFreezable(true)
 
 	self:SetUseType(SIMPLE_USE)
 
 	local phys = self:GetPhysicsObject()
 	phys:EnableMotion(false)
-	phys:EnableGravity(false)
 
 	local phys = self:GetPhysicsObject()
 	if (phys:IsValid()) then
@@ -30,8 +30,8 @@ function ENT:Use( activator, caller )
 	if activator:GetTardisData("interior") then
 		local pos,ang = TARDIS:GetLocalPos(self, activator)
 		local decimals = 3
-		local text =   "  Coords: ("..math.Round(pos.x,decimals)..", "..math.Round(pos.y,decimals)..", "..math.Round(pos.z,decimals).."),"
-		text = text .. "  Angle: ("..math.Round(ang.p,decimals)..", "..math.Round(ang.y,decimals)..", "..math.Round(ang.r,decimals)..")"
+		local text =   "  Vector("..math.Round(pos.x,decimals)..", "..math.Round(pos.y,decimals)..", "..math.Round(pos.z,decimals).."),"
+		text = text .. "  Angle("..math.Round(ang.p,decimals)..", "..math.Round(ang.y,decimals)..", "..math.Round(ang.r,decimals)..")"
 
 		activator:ChatPrint(" \nCurrent pointer position:")
 		activator:ChatPrint(text)
--- lua/entities/gmod_tardis_debug_pointer/shared.lua
+++ lua/entities/gmod_tardis_debug_pointer/shared.lua
@@ -9,17 +9,18 @@ ENT.Author			= "Brundoob"
 ENT.Purpose			= "Print locations in the TARDIS"
 ENT.Category		= "Doctor Who - TARDIS"
 ENT.Spawnable 		= false
-ENT.AdminOnly		= false
+ENT.AdminOnly		= true
 
 concommand.Add("tardis2_debug_pointer", function(ply,cmd,args)
+	if not (ply:IsAdmin() and gamemode.Call("PlayerSpawnSENT", ply, "gmod_tardis_debug_pointer")) then return end
 	local ent = ents.Create("gmod_tardis_debug_pointer")
 	ent:SetCreator(ply)
 
-	local tr = util.TraceLine( {
+	local tr = util.TraceLine({
 		start = ply:EyePos(),
 		endpos = ply:EyePos() + ply:EyeAngles():Forward() * 10000,
 		filter = function( ent ) if ( ent:GetClass() == "prop_physics" ) then return true end end
-	} )
+	})
 
 	local close_pos = ply:EyePos() + ply:EyeAngles():Forward() * 100
 
@@ -29,8 +30,10 @@ concommand.Add("tardis2_debug_pointer", function(ply,cmd,args)
 		ent:SetPos(close_pos)
 	end
 	ent:Spawn()
+	gamemode.Call("PlayerSpawnedSENT",ply,ent)
 	undo.Create("TARDIS Debug Pointer")
 	undo.AddEntity(ent)
 	undo.SetPlayer(ply)
 	undo.Finish()
+	ply:AddCleanup("sents",ent)
 end)
