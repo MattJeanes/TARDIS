d4507d521de8ae647352697d2037e5565d483645
Artiom Parygin <artiom.par@ya.ru>
2021-03-16 16:25:47 +0300

    Added debug pointer

---
+++ lua/entities/gmod_tardis_debug_pointer/init.lua
@@ -0,0 +1,43 @@
+-- TARDIS debug pointer
+-- Creators: Brundoob, Parar020100 and RyanM2711
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+ 
+include('shared.lua')
+ 
+function ENT:Initialize()
+ 
+	self:SetModel( "models/brundoob/precision.mdl" )
+	self:PhysicsInit( MOVETYPE_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetCollisionGroup( COLLISION_GROUP_WORLD )
+
+	self:SetUseType(SIMPLE_USE)
+
+	local phys = self:GetPhysicsObject()
+	phys:EnableMotion(false)
+	phys:EnableGravity(false)
+
+	local phys = self:GetPhysicsObject()
+	if (phys:IsValid()) then
+		phys:Wake()
+	end
+end
+
+function ENT:Use( activator, caller )
+	if activator:GetTardisData("interior") then
+		local pos,ang = TARDIS:GetLocalPos(self, activator)
+		local decimals = 3
+		local text =   "  Coords: ("..math.Round(pos.x,decimals)..", "..math.Round(pos.y,decimals)..", "..math.Round(pos.z,decimals).."),"
+		text = text .. "  Angle: ("..math.Round(ang.p,decimals)..", "..math.Round(ang.y,decimals)..", "..math.Round(ang.r,decimals)..")"
+
+		activator:ChatPrint(" \nCurrent pointer position:")
+		activator:ChatPrint(text)
+	end
+    return
+end
+ 
+function ENT:Think()
+end
---
+++ lua/entities/gmod_tardis_debug_pointer/shared.lua
@@ -0,0 +1,36 @@
+-- TARDIS debug pointer
+-- Creators: Brundoob, Parar020100 and RyanM2711
+
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "TARDIS Debug Pointer"
+ENT.Author			= "Brundoob"
+ENT.Purpose			= "Print locations in the TARDIS"
+ENT.Category		= "Doctor Who - TARDIS"
+ENT.Spawnable 		= false
+ENT.AdminOnly		= false
+
+concommand.Add("tardis2_debug_pointer", function(ply,cmd,args)
+	local ent = ents.Create("gmod_tardis_debug_pointer")
+	ent:SetCreator(ply)
+
+	local tr = util.TraceLine( {
+		start = ply:EyePos(),
+		endpos = ply:EyePos() + ply:EyeAngles():Forward() * 10000,
+		filter = function( ent ) if ( ent:GetClass() == "prop_physics" ) then return true end end
+	} )
+
+	local close_pos = ply:EyePos() + ply:EyeAngles():Forward() * 100
+
+	if ply:EyePos():Distance(tr.HitPos) < ply:EyePos():Distance(close_pos) then
+		ent:SetPos(tr.HitPos)
+	else
+		ent:SetPos(close_pos)
+	end
+	ent:Spawn()
+	undo.Create("TARDIS Debug Pointer")
+	undo.AddEntity(ent)
+	undo.SetPlayer(ply)
+	undo.Finish()
+end)
