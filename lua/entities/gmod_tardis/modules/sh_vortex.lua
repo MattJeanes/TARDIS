f02a629e8064a41b10b4019fb9c5730fa0703692
Matt Jeanes <matthew.jeanes@myairops.com>
2021-03-17 23:40:42 +0000

    Props attached to the tardis no longer prevent movement in the vortex and are visible in the vortex as well

--- lua/entities/gmod_tardis/modules/sh_vortex.lua
+++ lua/entities/gmod_tardis/modules/sh_vortex.lua
@@ -144,7 +144,24 @@ else
 	
 	local function dodraw(self,part)
 		render.SetBlend(1)
-		if self:GetData("vortexalpha",0) then
+		local vortexalpha = self:GetData("vortexalpha",0)
+		if vortexalpha>0 then
+			if not part and TARDIS:GetExteriorEnt()==self then
+				local attached = self:GetData("demat-attached")
+				if attached then
+					local oldblend = render.GetBlend()
+					render.SetBlend(vortexalpha)
+					for k,v in pairs(attached) do
+						if IsValid(k) and k.DrawModel and v>0 then
+							local oldc = k:GetColor()
+							k:SetColor(ColorAlpha(oldc,v))
+							k:DrawModel()
+							k:SetColor(oldc)
+						end
+					end
+					render.SetBlend(oldblend)
+				end
+			end
 			cam.IgnoreZ(false)
 		end
 	end
