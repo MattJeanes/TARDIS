311fe527bd0d42f8eacce7c31d442920d280c1e5
Artiom Parygin <artiom.par@ya.ru>
2021-03-20 17:57:50 +0300

    Added controls for opening all screens

--- lua/tardis/parts/default/gramophone.lua
+++ lua/tardis/parts/default/gramophone.lua
@@ -3,6 +3,7 @@
 local PART = {}
 PART.ID = "default_gramophone"
 PART.Name = "Default Gramophone"
+PART.Control = "music"
 PART.Text = "Gramophone"
 PART.Model = "models/drmatt/tardis/gramophone.mdl"
 PART.AutoSetup = true
@@ -10,8 +11,7 @@ PART.Collision = true
 
 if SERVER then
 	function PART:Use(ply)
-		if not ply:IsPlayer() then return end
-		TARDIS:PopToScreen("Music",ply)
+		TARDIS:Control("music", ply)
 	end
 end
 
