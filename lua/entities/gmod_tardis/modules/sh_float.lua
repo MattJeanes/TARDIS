f2fb811def2eeb7b2c9a197fd1ec809a365577d0
Artiom Parygin <artiom.par@ya.ru>
2021-03-21 16:58:49 +0300

    Messages and default control for float/anti-gravs

--- lua/entities/gmod_tardis/modules/sh_float.lua
+++ lua/entities/gmod_tardis/modules/sh_float.lua
@@ -3,7 +3,11 @@
 TARDIS:AddControl({
 	id = "float",
 	ext_func=function(self,ply)
-		self:ToggleFloat()
+		if self:ToggleFloat() then
+			TARDIS:StatusMessage(ply, "Anti-gravs", self:GetData("float"))
+		else
+			TARDIS:ErrorMessage(ply, "Failed to toggle anti-gravs")
+		end
 	end,
 	serveronly=true,
 	screen_button = {
--- lua/tardis/parts/default/float.lua
+++ lua/tardis/parts/default/float.lua
@@ -10,14 +10,4 @@ PART.Collision = true
 PART.Animate = true
 PART.Sound = "drmatt/tardis/default/control_handbrake.wav"
 
-if SERVER then
-	function PART:Use(ply)
-		if self.exterior:ToggleFloat() then
-			ply:ChatPrint("Anti-gravs ".. (self.exterior:GetData("float") and "enabled" or "disabled"))
-		else
-			ply:ChatPrint("Failed to toggle anti-gravs")
-		end
-	end
-end
-
 TARDIS:AddPart(PART)
