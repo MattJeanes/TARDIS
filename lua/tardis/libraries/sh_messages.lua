98f96dbe1418aa4ecec2f8d603d13a6e9b1036b4
Artiom Parygin <artiom.par@ya.ru>
2021-03-21 12:05:34 +0300

    Optimised debug convar

--- lua/tardis/libraries/sh_messages.lua
+++ lua/tardis/libraries/sh_messages.lua
@@ -1,14 +1,22 @@
 CreateConVar("tardis2_debug", 0, {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "TARDIS - debug enabled")
 
+TARDIS.debug = GetConVar("tardis2_debug"):GetBool()
+
+cvars.AddChangeCallback("tardis2_debug", function()
+	TARDIS.debug = GetConVar("tardis2_debug"):GetBool()
+end)
+
 function TARDIS:IsDebugOn()
-	return GetConVar("tardis2_debug"):GetBool()
+	return TARDIS.debug
 end
 
-function TARDIS:Debug(text)
+function TARDIS:Debug(text, ply)
 	if TARDIS:IsDebugOn() then
-		local fulltext = "[TARDIS Debug]"..text
+		local fulltext = "[TARDIS Debug] "..text
 		if CLIENT and LocalPlayer() then
 			LocalPlayer():ChatPrint(fulltext)
+		elseif ply ~= nil then
+			ply:ChatPrint(fulltext)
 		else
 			print(fulltext)
 		end
