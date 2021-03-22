ac1381c06df1567e21463e3ff3c552e37eb61d76
Win7yes <francodmeneguzzi@gmail.com>
2021-03-22 19:51:41 -0300

    Add default interior doorframe

--- lua/tardis/interiors/default.lua
+++ lua/tardis/interiors/default.lua
@@ -90,6 +90,10 @@ T.Interior={
 				view_range_min = 50
 			},
 		},
+		default_doorframe = {
+			pos = Vector(317,334,-89),
+			ang = Angle(0,-40,0)
+		}
 	},
 	Tips={
 		view_range_min=70,
---
+++ lua/tardis/parts/default/doorframe.lua
@@ -0,0 +1,12 @@
+-- Default Interior - Door frame
+
+local PART = {}
+PART.ID = "default_doorframe"
+PART.Name = "Default Door Frame"
+PART.Model = "models/drmatt/tardis/door.mdl"
+PART.BypassIsomorphic = true
+PART.AutoSetup = true
+PART.Collision = true
+PART.Animate = true
+
+TARDIS:AddPart(PART)
