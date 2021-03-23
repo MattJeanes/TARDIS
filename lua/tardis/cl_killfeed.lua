11211fdf9cbb43d89ed954849b59e727a5a36be7
Dodo <theplumo@gmail.com>
2021-03-23 06:43:45 +0000

    Update lua/tardis/cl_killfeed.lua
    Co-authored-by: Franco D. M. <7284418+francodml@users.noreply.github.com>

--- lua/tardis/cl_killfeed.lua
+++ lua/tardis/cl_killfeed.lua
@@ -1,4 +1,2 @@
-if CLIENT then
-    killicon.Add( "gmod_tardis", "killfeed/gmod_tardis", Color( 255, 255, 255, 255 ))
-    language.Add("gmod_tardis", "TARDIS")
-end
+killicon.Add( "gmod_tardis", "killfeed/gmod_tardis", Color( 255, 255, 255, 255 ))
+language.Add("gmod_tardis", "TARDIS")
