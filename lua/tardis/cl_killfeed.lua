989dc2cf6e92c099076aa6d90aca085faa145a2e
Matt Jeanes <matthew.jeanes@myairops.com>
2021-03-23 15:52:02 +0000

    minor formatting

--- lua/tardis/cl_killfeed.lua
+++ lua/tardis/cl_killfeed.lua
@@ -1,2 +1,2 @@
-killicon.Add( "gmod_tardis", "killfeed/gmod_tardis", Color( 255, 255, 255, 255 ))
+killicon.Add("gmod_tardis", "killfeed/gmod_tardis", Color(255,255,255,255))
 language.Add("gmod_tardis", "TARDIS")
