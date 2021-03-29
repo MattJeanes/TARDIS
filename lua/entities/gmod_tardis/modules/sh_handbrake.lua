ea2f200b777fea2c96e28c83c3ddf8b042aed3a6
Artiom Parygin <artiom.par@ya.ru>
2021-03-29 13:56:24 +0300

    Handbrake control

--- lua/entities/gmod_tardis/modules/sh_handbrake.lua
+++ lua/entities/gmod_tardis/modules/sh_handbrake.lua
@@ -4,9 +4,11 @@
 TARDIS:AddControl({
 	id = "handbrake",
 	ext_func=function(self, ply)
-		-- Code will be added here
-
-		TARDIS:ErrorMessage(ply, "Handbrake is not yet implemented")
+		if self:ToggleHandbrake() then
+			TARDIS:StatusMessage(ply, "Time Rotor Handbrake", self:GetData("handbrake"), "engaged", "disengaged")
+		else
+			TARDIS:ErrorMessage(ply, "Failed to toggle the Time Rotor Handbrake")
+		end
 	end,
 	serveronly=true,
 	screen_button = {
@@ -14,10 +16,38 @@ TARDIS:AddControl({
 		mmenu = false,
 		toggle = true,
 		frame_type = {0, 2},
-		text = "Time-Rotor Handbrake",
+		text = "Time Rotor Handbrake",
 		pressed_state_from_interior = false,
 		pressed_state_data = "handbrake", -- can be changed
 		order = 12,
 	},
-	tip_text = "Time-Rotor Handbrake",
+	tip_text = "Time Rotor Handbrake",
 })
+
+ENT:AddHook("Initialize","handbrake-init", function(self)
+	self:SetData("handbrake", false, true)
+end)
+
+if SERVER then
+	function ENT:ToggleHandbrake()
+		return self:SetHandbrake(not self:GetData("handbrake"))
+	end
+	function ENT:SetHandbrake(on)
+		if (self:CallHook("CanToggleHandbrake") == false
+			or self.interior:CallHook("CanToggleHandbrake")) == false
+		then
+			return false
+		end
+		self:SetData("handbrake", on, true)
+		self:CallHook("HandbrakeToggled", on)
+		if self.interior then
+			self.interior:CallHook("HandbrakeToggled", on)
+		end
+		return true
+	end
+
+	ENT:AddHook("FailDemat", "handbrake", function(self)
+		if self:GetData("handbrake") then return true end
+	end)
+
+end
