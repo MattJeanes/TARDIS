b6cbcf365e4c0f8aaf780984bb0a6133b7edfaa8
Artiom Parygin <artiom.par@ya.ru>
2021-11-11 23:48:56 +0300

    Added sonic to the default interior

---
+++ lua/tardis/parts/default/sonic_dispenser_button.lua
@@ -0,0 +1,12 @@
+-- Default Interior - Sonic charger
+
+local PART = {}
+PART.ID = "default_sonicdispenser"
+PART.Name = "Default Sonic Screwdriver Dispenser"
+PART.Control = "sonic_dispenser"
+PART.AutoSetup = true
+PART.Collision = true
+PART.Animate = true
+PART.Model = "models/drmatt/tardis/smallbutton.mdl"
+
+TARDIS:AddPart(PART)
