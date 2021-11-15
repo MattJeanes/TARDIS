a9f7defe44749388ee572340bb2a0c7a8e41266d
Artiom Parygin <artiom.par@ya.ru>
2021-11-15 13:05:54 +0300

    client / server code refactored

--- lua/entities/gmod_tardis/modules/sh_handbrake.lua
+++ lua/entities/gmod_tardis/modules/sh_handbrake.lua
@@ -2,35 +2,35 @@ ENT:AddHook("Initialize","handbrake-init", function(self)
 	self:SetData("handbrake", false, true)
 end)
 
-if SERVER then
-	function ENT:ToggleHandbrake()
-		return self:SetHandbrake(not self:GetData("handbrake"))
+if CLIENT then return end
+
+function ENT:ToggleHandbrake()
+	return self:SetHandbrake(not self:GetData("handbrake"))
+end
+function ENT:SetHandbrake(on)
+	if (self:CallHook("CanToggleHandbrake") == false
+		or self.interior:CallHook("CanToggleHandbrake")) == false
+	then
+		return false
 	end
-	function ENT:SetHandbrake(on)
-		if (self:CallHook("CanToggleHandbrake") == false
-			or self.interior:CallHook("CanToggleHandbrake")) == false
-		then
-			return false
-		end
-		self:SetData("handbrake", on, true)
-		self:CallHook("HandbrakeToggled", on)
-		if self.interior then
-			self.interior:CallHook("HandbrakeToggled", on)
-		end
+	self:SetData("handbrake", on, true)
+	self:CallHook("HandbrakeToggled", on)
+	if self.interior then
+		self.interior:CallHook("HandbrakeToggled", on)
+	end
+	return true
+end
+
+ENT:AddHook("FailDemat", "handbrake", function(self, force)
+	if self:GetData("handbrake") and force ~= true then
 		return true
 	end
+end)
 
-	ENT:AddHook("FailDemat", "handbrake", function(self, force)
-		if self:GetData("handbrake") and force ~= true then
-			return true
+ENT:AddHook("HandbrakeToggled", "vortex", function(self, force)
+	if self:GetData("handbrake") then
+		if self:GetData("teleport") or self:GetData("vortex") then
+			self:InterruptTeleport()
 		end
-	end)
-
-	ENT:AddHook("HandbrakeToggled", "vortex", function(self, force)
-		if self:GetData("handbrake") then
-			if self:GetData("teleport") or self:GetData("vortex") then
-				self:InterruptTeleport()
-			end
-		end
-	end)
-end
+	end
+end)
