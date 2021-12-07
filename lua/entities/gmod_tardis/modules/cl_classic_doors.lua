630a2d0c553615aa379cc53526113484ff3809a1
Artiom Parygin <artiom.par@ya.ru>
2021-12-07 07:04:44 +0300

    Classic doors support with hooks

--- lua/entities/gmod_tardis/modules/cl_classic_doors.lua
+++ lua/entities/gmod_tardis/modules/cl_classic_doors.lua
@@ -1,35 +1,29 @@
-local function SetVisibleIfExists(part, visible)
-	if part ~= nil and part ~= NULL then
-		if visible then
-			part:SetColor(Color(255,255,255,255))
-			part:SetRenderMode(RENDERMODE_NORMAL)
-		else
-			part:SetRenderMode(RENDERMODE_TRANSALPHA)
-			part:SetColor(Color(0,0,0,0))
-		end
-	end
-end
-
-ENT:AddHook("Think", "classic_doors", function(self)
+ENT:AddHook("ShouldDraw", "classic_doors_support", function(self)
 	if self.metadata.EnableClassicDoors ~= true then return end
 
-	local door = TARDIS:GetPart(self,"door")
-	local door_mirror = TARDIS:GetPart(self.interior,"door")
-	local intdoor = TARDIS:GetPart(self.interior,"intdoor")
 	local ply = LocalPlayer()
 
-	if ply:GetTardisData("exterior") == self
+	local inside = (ply:GetTardisData("exterior") == self
 		and not ply:GetTardisData("thirdperson")
-		and not ply:GetTardisData("destination")
-	then
-		SetVisibleIfExists(self, false)
-		SetVisibleIfExists(door, false)
-		SetVisibleIfExists(door_mirror, false)
-		SetVisibleIfExists(intdoor, true)
-	else
-		SetVisibleIfExists(self, true)
-		SetVisibleIfExists(door, true)
-		SetVisibleIfExists(door_mirror, true)
-		SetVisibleIfExists(intdoor, false)
+		and not ply:GetTardisData("destination"))
+
+	if inside then return false end
+
+end)
+
+ENT:AddHook("ShouldDrawPart", "classic_doors_support", function(self, part)
+	if self.metadata.EnableClassicDoors ~= true then return end
+	if part == nil then return end
+
+	local ply = LocalPlayer()
+
+	local inside = (ply:GetTardisData("exterior") == self
+		and not ply:GetTardisData("thirdperson")
+		and not ply:GetTardisData("destination"))
+
+	if part == TARDIS:GetPart(self,"door") then
+		if inside then
+			return false
+		end
 	end
 end)
---
+++ lua/entities/gmod_tardis_interior/modules/cl_classic_doors.lua
@@ -0,0 +1,20 @@
+ENT:AddHook("ShouldDrawPart", "classic_doors_support", function(self, part)
+	if self.metadata.EnableClassicDoors ~= true then return end
+	if part == nil then return end
+
+	local ply = LocalPlayer()
+
+	local inside = (ply:GetTardisData("interior") == self
+		and not ply:GetTardisData("thirdperson")
+		and not ply:GetTardisData("destination"))
+
+	--TARDIS:Debug("A")
+
+	if inside and part == TARDIS:GetPart(self, "door") then
+		return false
+	end
+	if not inside and part == TARDIS:GetPart(self, "intdoor") then
+		return false
+	end
+
+end)
