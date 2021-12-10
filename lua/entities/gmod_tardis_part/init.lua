51203363b61dfc8b675ce4306e849bfa0119c86f
Artiom Parygin <artiom.par@ya.ru>
2021-12-10 13:55:53 +0300

    SetCollide() function

--- lua/entities/gmod_tardis_part/init.lua
+++ lua/entities/gmod_tardis_part/init.lua
@@ -7,3 +7,13 @@ function ENT:OnTakeDamage(dmginfo)
 	if self.parent:CallHook("ShouldTakeDamage",dmginfo)==false then return end
 	self.parent:CallHook("OnTakeDamage", dmginfo)
 end
+
+function ENT:SetCollide(collide, notrace)
+	if collide then
+		self:SetCollisionGroup(COLLISION_GROUP_NONE)
+	elseif notrace then
+		self:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
+	else
+		self:SetCollisionGroup(COLLISION_GROUP_WORLD)
+	end
+end
