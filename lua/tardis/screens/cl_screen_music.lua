270d26bd05bbf928f1181da5d52e9ab8351d028f
Artiom Parygin <artiom.par@ya.ru>
2022-01-09 23:07:59 +0300

    Refactoring of the music screen

--- lua/tardis/screens/cl_screen_music.lua
+++ lua/tardis/screens/cl_screen_music.lua
@@ -60,147 +60,145 @@ function TARDIS:RemoveCustomMusic(index)
 end
 
 
-	--Music GUI
-
+-- Music GUI
 TARDIS:AddScreen("Music", {id="music", menu=false, order=10, popuponly=true}, function(self,ext,int,frame,screen)
+
+--------------------------------------------------------------------------------
+-- Layout calculations
+--------------------------------------------------------------------------------
 	local frW = frame:GetWide()
 	local frT = frame:GetTall()
-	local gap = math.min(frT, frW) * 0.05 * 1.2
 
-	local bT = frT * 0.1
+	local gap = math.min(frT, frW) * 0.05 * 1.2
 
 	local listW = frW * 0.3
 	local listT = frT - 2 * gap
-
 	local tbW = frW - 4 * gap - 2 * listW
 	local tbT = frT * 0.1
 	local bW = 0.5 * (tbW - gap)
+	local bT = frT * 0.1
 
 	local midX = 3 * gap + 2 * listW
 
-	local url
-	local name_bar
-	local list_premade
-	local list_custom
-
-	local text_bar = vgui.Create( "DTextEntry", frame )
-	text_bar:SetPlaceholderText("Enter song URL (Clientside Only)")
-	text_bar:SetFont(TARDIS:GetScreenFont(screen, "Default"))
-	text_bar:SetSize(tbW, tbT)
-	text_bar:SetPos(midX, gap)
-	function text_bar:OnEnter()
-		ext:PlayMusic(text_bar:GetValue())
-	end
-
-	function text_bar:OnGetFocus()
-		text_bar:SetTextColor(Color(0,0,0))
-		name_bar:SetTextColor(Color(0,0,0))
-		list_premade:ClearSelection()
-		list_custom:ClearSelection()
-	end
-
-
-	function text_bar:OnValueChange()
-		text_bar:SetTextColor(Color(0,0,0))
-		name_bar:SetTextColor(Color(0,0,0))
-		list_premade:ClearSelection()
-		list_custom:ClearSelection()
-	end
-
-	name_bar = vgui.Create( "DTextEntry", frame )
-	name_bar:SetPlaceholderText("Enter custom song name")
-	name_bar:SetFont(TARDIS:GetScreenFont(screen, "Default"))
-	name_bar:SetSize(tbW, tbT)
-	name_bar:SetPos(midX, 2 * gap + tbT)
-	function name_bar:OnEnter()
-		TARDIS:AddCustomMusic(name_bar:GetText(), text_bar:GetText())
-		frame.updatelist()
-	end
-
-	function name_bar:OnGetFocus()
-		text_bar:SetTextColor(Color(0,0,0))
-		name_bar:SetTextColor(Color(0,0,0))
-		list_premade:ClearSelection()
-		list_custom:ClearSelection()
-	end
-
-	function name_bar:OnValueChange()
-		text_bar:SetTextColor(Color(0,0,0))
-		name_bar:SetTextColor(Color(0,0,0))
-		list_premade:ClearSelection()
-		list_custom:ClearSelection()
-	end
-
-	--Buttons
-
-	local playbutton=vgui.Create("DButton",frame)
-	playbutton:SetSize(tbW, bT * 1.3)
-	playbutton:SetPos(midX, gap + listT - bT * 1.3)
-	playbutton:SetText("Play / Stop")
-	playbutton:SetFont(TARDIS:GetScreenFont(screen, "Default"))
-	function playbutton:DoClick()
-		if IsValid(ext.music) and ext.music:GetState()==GMOD_CHANNEL_PLAYING then
-			ext:StopMusic()
-		else
-			if list_premade:GetSelectedLine() then
-				ext:PlayMusic(url)
-			else
-				ext:PlayMusic(text_bar:GetValue())
-			end
-		end
-	end
-
-	local removemus=vgui.Create("DButton",frame)
-	removemus:SetSize(bW, bT)
-	removemus:SetPos(midX + gap + bW, 3 * gap + 2 * tbT)
-	removemus:SetText("Remove")
-	removemus:SetFont(TARDIS:GetScreenFont(screen, "Default"))
-
-	--Pre-loaded legacy music select
-
-	list_premade = vgui.Create("DListView",frame)
+--------------------------------------------------------------------------------
+-- Layout
+--------------------------------------------------------------------------------
+	local list_premade = vgui.Create("DListView",frame)
 	list_premade:SetSize(listW, listT)
 	list_premade:SetPos(gap, gap)
 	list_premade:AddColumn("Pre-loaded music")
 	list_premade:SetMultiSelect(false)
-	for k,v in pairs(default_music) do
-		list_premade:AddLine(v[1])
-	end
 
-	function list_premade:OnRowSelected(rowIndex, row)
-		list_custom:ClearSelection()
-		url = ("https://mattjeanes.com/data/tardis/" .. default_music[rowIndex][2] ..".mp3")
-		text_bar:SetTextColor(Color(139,139,139))
-		name_bar:SetTextColor(Color(139,139,139))
-	end
-
-	--Custom music select
-
-	list_custom = vgui.Create("DListView",frame)
+	local list_custom = vgui.Create("DListView",frame)
 	list_custom:SetSize(listW, listT)
 	list_custom:SetPos(2 * gap + listW, gap)
 	list_custom:AddColumn("Custom Music")
 	list_custom:SetMultiSelect(false)
 
+	local url_bar = vgui.Create( "DTextEntry", frame )
+	url_bar:SetPlaceholderText("Enter song URL (Clientside Only)")
+	url_bar:SetFont(TARDIS:GetScreenFont(screen, "Default"))
+	url_bar:SetSize(tbW, tbT)
+	url_bar:SetPos(midX, gap)
+
+	local name_bar = vgui.Create( "DTextEntry", frame )
+	name_bar:SetPlaceholderText("Enter custom song name")
+	name_bar:SetFont(TARDIS:GetScreenFont(screen, "Default"))
+	name_bar:SetSize(tbW, tbT)
+	name_bar:SetPos(midX, 2 * gap + tbT)
+
+	local play_stop_button=vgui.Create("DButton",frame)
+	play_stop_button:SetSize(tbW, bT * 1.3)
+	play_stop_button:SetPos(midX, gap + listT - bT * 1.3)
+	play_stop_button:SetText("Play / Stop")
+	play_stop_button:SetFont(TARDIS:GetScreenFont(screen, "Default"))
+
+	local save_custom_button=vgui.Create("DButton",frame)
+	save_custom_button:SetSize(bW, bT)
+	save_custom_button:SetPos(midX, 3 * gap + 2 * tbT)
+	save_custom_button:SetText("Save")
+	save_custom_button:SetFont(TARDIS:GetScreenFont(screen, "Default"))
+
+	local remove_custom_button=vgui.Create("DButton",frame)
+	remove_custom_button:SetSize(bW, bT)
+	remove_custom_button:SetPos(midX + gap + bW, 3 * gap + 2 * tbT)
+	remove_custom_button:SetText("Remove")
+	remove_custom_button:SetFont(TARDIS:GetScreenFont(screen, "Default"))
+
+--------------------------------------------------------------------------------
+-- Loading data
+--------------------------------------------------------------------------------
+	local url = ""
+
+	for k,v in pairs(default_music) do
+		list_premade:AddLine(v[1])
+	end
+
+	function list_custom:UpdateAll()
+		self:Clear()
+		if url_bar ~= nil then
+			for k,v in pairs(custom_music) do
+				self:AddLine(v[1])
+			end
+		end
+	end
+
+	list_custom:UpdateAll()
+
+--------------------------------------------------------------------------------
+-- Selecting the rows
+--------------------------------------------------------------------------------
+
 	function list_custom:OnRowSelected(rowIndex,row)
 		list_premade:ClearSelection()
-		text_bar:SetText(custom_music[rowIndex][2])
+		url_bar:SetText(custom_music[rowIndex][2])
 		name_bar:SetText(custom_music[rowIndex][1])
-		text_bar:SetTextColor(Color(0,0,0))
+		url_bar:SetTextColor(Color(0,0,0))
 		name_bar:SetTextColor(Color(0,0,0))
 	end
 
-	local savemus=vgui.Create("DButton",frame)
-	savemus:SetSize(bW, bT)
-	savemus:SetPos(midX, 3 * gap + 2 * tbT)
-	savemus:SetText("Save")
-	savemus:SetFont(TARDIS:GetScreenFont(screen, "Default"))
-	function savemus:DoClick()
-		TARDIS:AddCustomMusic(name_bar:GetText(), text_bar:GetText())
-		frame.updatelist()
+	function list_premade:OnRowSelected(rowIndex, row)
+		list_custom:ClearSelection()
+		url = ("https://mattjeanes.com/data/tardis/" .. default_music[rowIndex][2] ..".mp3")
+		url_bar:SetTextColor(Color(139,139,139))
+		name_bar:SetTextColor(Color(139,139,139))
 	end
 
-	function removemus:DoClick()
+	local function highlight_custom()
+		url_bar:SetTextColor(Color(0,0,0))
+		name_bar:SetTextColor(Color(0,0,0))
+		list_premade:ClearSelection()
+		list_custom:ClearSelection()
+	end
+
+	function url_bar:OnGetFocus()
+		highlight_custom()
+	end
+	function url_bar:OnValueChange()
+		highlight_custom()
+	end
+	function name_bar:OnGetFocus()
+		highlight_custom()
+	end
+	function name_bar:OnValueChange()
+		highlight_custom()
+	end
+
+--------------------------------------------------------------------------------
+-- Add / remove custom music
+--------------------------------------------------------------------------------
+
+	function name_bar:OnEnter()
+		TARDIS:AddCustomMusic(name_bar:GetText(), url_bar:GetText())
+		list_custom:UpdateAll()
+	end
+
+	function save_custom_button:DoClick()
+		TARDIS:AddCustomMusic(name_bar:GetText(), url_bar:GetText())
+		list_custom:UpdateAll()
+	end
+
+	function remove_custom_button:DoClick()
 		local line = list_custom:GetSelectedLine()
 		if not line then
 			TARDIS:ErrorMessage(LocalPlayer(), "Nothing has been chosen for removal.")
@@ -212,21 +210,31 @@ TARDIS:AddScreen("Music", {id="music", menu=false, order=10, popuponly=true}, fu
 					"Yes",
 					function()
 						TARDIS:RemoveCustomMusic(line)
-						frame.updatelist()
+						list_custom:UpdateAll()
 					end,
 					"No",
 					function()
 					end):SetSkin("TARDIS")
 	end
 
-	function frame.updatelist()
-		list_custom:Clear()
-		if text_bar ~= nil then
-			for k,v in pairs(custom_music) do
-				list_custom:AddLine(v[1])
+--------------------------------------------------------------------------------
+-- Play music
+--------------------------------------------------------------------------------
+
+	function url_bar:OnEnter()
+		ext:PlayMusic(url_bar:GetValue())
+	end
+
+	function play_stop_button:DoClick()
+		if IsValid(ext.music) and ext.music:GetState()==GMOD_CHANNEL_PLAYING then
+			ext:StopMusic()
+		else
+			if list_premade:GetSelectedLine() then
+				ext:PlayMusic(url)
+			else
+				ext:PlayMusic(url_bar:GetValue())
 			end
 		end
 	end
-	frame.updatelist()
 
 end)
