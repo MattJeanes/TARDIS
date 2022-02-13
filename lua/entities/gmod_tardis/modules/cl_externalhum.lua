21674434fa258b59742c8666c3d29b50f557983e
Creth <Kylebugg1@outlook.com>
2022-02-13 18:00:06 +1000

    Added support for external hum

---
+++ lua/entities/gmod_tardis/modules/cl_externalhum.lua
@@ -0,0 +1,34 @@
+-- Idle sound
+
+TARDIS:AddSetting({
+    id="external_hum",
+    name="External Hum",
+    desc="Whether the hum on the outside of the TARDIS can be heard if it exists",
+    section="Sounds",
+    value=true,
+    type="bool",
+    option=true
+})
+
+ENT:AddHook("OnRemove", "externalhum", function(self)
+    if self.ExternalHum then
+        self.ExternalHum:Stop()
+        self.ExternalHum = nil
+    end
+end)
+
+ENT:AddHook("Think", "externalhum", function(self)
+    local hum_sound = self.metadata.Exterior.Sounds.Hum
+    if hum_sound then
+        if TARDIS:GetSetting("external_hum") and TARDIS:GetSetting("sound") then
+            if not self.ExternalHum then
+                self.ExternalHum = CreateSound(self, hum_sound.path)
+                self.ExternalHum:Play()
+                self.ExternalHum:ChangeVolume(hum_sound.volume or 1,0)
+            end
+        elseif self.ExternalHum then
+            self.ExternalHum:Stop()
+            self.ExternalHum=nil
+        end
+    end
+end)
