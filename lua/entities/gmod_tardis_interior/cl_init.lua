c93ea09bfba6888d66d1472cc3497477d9f58592
Artiom Parygin <artiom.par@ya.ru>
2022-02-15 20:01:10 +0300

    Fix: clientside templates

--- lua/entities/gmod_tardis_interior/cl_init.lua
+++ lua/entities/gmod_tardis_interior/cl_init.lua
@@ -2,7 +2,8 @@ include('shared.lua')
 
 ENT:AddHook("PlayerInitialize", "interior", function(self)
     self.metadata=TARDIS:GetInterior(net.ReadString())
-    
+    self.metadata = TARDIS:MergeInteriorTemplates(self.metadata, true, self:GetCreator())
+
     self.Model=self.metadata.Interior.Model
     self.Fallback=self.metadata.Interior.Fallback
     self.Portal=self.metadata.Interior.Portal
