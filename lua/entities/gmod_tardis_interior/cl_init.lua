17f3445e9188d924e71ea6c72fbe916898c4e639
artixc <mr.dancinglights@gmail.com>
2022-02-20 19:17:34 -0500

    Fixed Query

--- lua/entities/gmod_tardis_interior/cl_init.lua
+++ lua/entities/gmod_tardis_interior/cl_init.lua
@@ -8,7 +8,7 @@ ENT:AddHook("PlayerInitialize", "interior", function(self)
     self.Portal=self.metadata.Interior.Portal
     self.ExitDistance=self.metadata.Interior.ExitDistance
     if GetConVarNumber("r_rootlod")>0 then
-		Derma_Query("The TARDIS Interior requires model detail on high, set now?", "TARDIS Interior", "Yes", function() RunConsoleCommand("r_rootlod", 0) end, "No", function() end)
+		Derma_Query("The TARDIS Interior might require model detail on high, set now?", "TARDIS Interior", "Yes", function() RunConsoleCommand("r_rootlod", 0) end, "No", function() end)
 	end
 end)
 
