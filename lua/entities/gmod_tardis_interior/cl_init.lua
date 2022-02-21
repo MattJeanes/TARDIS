252ccf991b38e3fc9b61a569cc05ebcb779ac203
artixc <mr.dancinglights@gmail.com>
2022-02-20 19:12:50 -0500

    Added Prompt

--- lua/entities/gmod_tardis_interior/cl_init.lua
+++ lua/entities/gmod_tardis_interior/cl_init.lua
@@ -7,4 +7,8 @@ ENT:AddHook("PlayerInitialize", "interior", function(self)
     self.Fallback=self.metadata.Interior.Fallback
     self.Portal=self.metadata.Interior.Portal
     self.ExitDistance=self.metadata.Interior.ExitDistance
+    if GetConVarNumber("r_rootlod")>0 then
+		Derma_Query("The TARDIS Interior requires model detail on high, set now?", "TARDIS Interior", "Yes", function() RunConsoleCommand("r_rootlod", 0) end, "No", function() end)
+	end
 end)
+
