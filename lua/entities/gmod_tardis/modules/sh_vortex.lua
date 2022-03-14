c687ea462d26234b2846d73f237791c0fd91c84b
Artiom Parygin <artiom.par@ya.ru>
2022-03-14 11:42:07 +0300

    Formatting fixed

--- lua/entities/gmod_tardis/modules/sh_vortex.lua
+++ lua/entities/gmod_tardis/modules/sh_vortex.lua
@@ -3,7 +3,9 @@
 function ENT:IsVortexEnabled(pilot)
     local hookResult = self:CallHook("VortexEnabled", pilot)
     if hookResult ~= nil then return hookResult end
-    return ((not pilot and SERVER) or TARDIS:GetSetting("vortex-enabled", pilot)) and IsValid(self:GetPart("vortex")) and (SERVER or self:GetData("vortexmodelvalid"))
+    return ( ((not pilot and SERVER) or TARDIS:GetSetting("vortex-enabled", pilot))
+            and IsValid(self:GetPart("vortex"))
+            and (SERVER or self:GetData("vortexmodelvalid")) )
 end
 
 ENT:AddHook("VortexEnabled", "demat-fast", function(self, pilot)
