a5962624e8eb7ea1926dce2aa244dc6d96adc81a
Artiom Parygin <artiom.par@ya.ru>
2022-04-03 13:42:24 +0300

    Lamps configurable by metadata

--- lua/entities/gmod_tardis_interior/modules/sh_interior_lights.lua
+++ lua/entities/gmod_tardis_interior/modules/sh_interior_lights.lua
@@ -70,37 +70,61 @@ if CLIENT then
 else -- SERVER
     
     if SERVER then
-        ENT:AddHook("Initialize", "lights-projected", function(self)
-            --[[local pl = ProjectedTexture()
-            pl:SetTexture(self.metadata.Exterior.ProjectedLight.texture)
-            pl:SetVerticalFOV(self.metadata.Exterior.ProjectedLight.vertfov or self.metadata.Exterior.Portal.height)
-            pl:SetHorizontalFOV(self.metadata.Exterior.ProjectedLight.horizfov or self.metadata.Exterior.Portal.width+10)
-            pl:SetEnableShadows(true)
-            pl:SetPos(self:LocalToWorld(Vector(-110.811, 19.174, 138.526)))
-            pl:SetColor(Color(255,255,255))
-            pl:SetFarZ(100)
-            pl:SetAngles(Angle(0,0,0))
-            pl:SetBrightness(100)
-            pl:Update()
-            tardisdebug("PL")]]
-    
-            local lamp = MakeLamp(self:GetCreator(),
-                    255, 255, 255, KEY_NONE, true,
-                    "effects/flashlight/soft",
+        ENT:AddHook("Initialize", "lamps", function(self)
+            local lamps = self.metadata.Interior.Lamps
+            if not lamps then return end
+
+            self.lamps = {}
+
+            for k,v in pairs(lamps) do
+                local lamp = MakeLamp(self:GetCreator(),
+                    v.color.r, v.color.g, v.color.b, KEY_NONE, true,
+                    v.texture or "effects/flashlight/soft",
                     "models/maxofs2d/lamp_projector.mdl",
-                    9920, 1024, 4, true,
+                    v.fov or 90, v.distance or 1024, v.brightness or 3.0, true,
                     {
-                        Pos = self:LocalToWorld(Vector(-110.811, 19.174, 138.526)),
-                        Angle = Angle(0,0,0),
+                        Pos = self:LocalToWorld(v.pos or Vector(0,0,0)),
+                        Angle = v.ang or Angle(0,0,0),
                     })
 
-            lamp:SetRenderMode(RENDERMODE_TRANSALPHA)
-            lamp:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
-            lamp:SetUnFreezable(false)
-            lamp:GetPhysicsObject():EnableMotion(false)
-            lamp:SetColor(Color(255,255,0,0))
+                lamp:SetUnFreezable(false)
+                lamp:GetPhysicsObject():EnableGravity(false)
+                lamp:GetPhysicsObject():EnableMotion(false)
+
+                if self.metadata.Interior.LampsDebug == true then
+                    lamp:SetUseType(SIMPLE_USE)
+                    lamp.Use = function(ply)
+                        local clr = lamp:GetColor()
+                        local pos = self:WorldToLocal(lamp:GetPos())
+                        local ang = lamp:GetAngles()
+                        print("{\n\tcolor = Color(" .. clr.r .. ", " .. clr.g .. ", " .. clr.b .. "),")
+                        print("\ttexture = \"" .. lamp:GetFlashlightTexture() .. "\",")
+                        print("\tfov = " .. lamp:GetLightFOV() .. ",")
+                        print("\tdistance = " .. lamp:GetDistance() .. ",")
+                        print("\tbrightness = " .. lamp:GetBrightness() .. ",")
+                        print("\tpos = Vector(" .. pos.x .. ", " .. pos.y .. ", " .. pos.z .. "),")
+                        print("\tang = Angle(" .. ang.x .. ", " .. ang.y .. ", " .. ang.z .. "),")
+                        print("},")
+                    end
+                    lamp:SetCollisionGroup(COLLISION_GROUP_WORLD)
+                else
+                    lamp:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
+                    lamp:SetRenderMode(RENDERMODE_TRANSALPHA)
+                    lamp:SetColor(Color(v.color.r, v.color.g, v.color.b,0))
+                end
+
+                table.insert(self.lamps, lamp)
+            end
         end)
     end
 
+    ENT:AddHook("OnRemove", "music", function(self)
+        if not self.lamps then return end
+        for k,v in pairs(self.lamps) do
+            if IsValid(v) then
+                v:Remove()
+            end
+        end
+    end)
 
 end
