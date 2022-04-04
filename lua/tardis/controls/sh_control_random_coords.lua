882797c82682071713c7e0ca2978abb02aeec4d7
Cem111333 <reisoglu.cem@gmail.com>
2022-04-04 17:28:46 +0100

    Random Coords Notifications

--- lua/tardis/controls/sh_control_random_coords.lua
+++ lua/tardis/controls/sh_control_random_coords.lua
@@ -2,9 +2,10 @@ TARDIS:AddControl({
     id = "random_coords",
     ext_func=function(self,ply)
         self:SetRandomDestination(true)
+        TARDIS:Message(ply, "Random destination has been selected")
     end,
     serveronly=true,
     power_independent = true,
     screen_button = false,
-    tip_text = "Randomised Coordinates",
+    tip_text = "Random Destination",
 })
