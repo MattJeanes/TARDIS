fde75e9aa0db927ea9c8bbf984b280d6f5255d45
Cem111333 <reisoglu.cem@gmail.com>
2022-04-04 17:52:06 +0100

    floats define locations with random destination.

--- lua/tardis/controls/sh_control_random_coords.lua
+++ lua/tardis/controls/sh_control_random_coords.lua
@@ -1,7 +1,8 @@
 TARDIS:AddControl({
     id = "random_coords",
     ext_func=function(self,ply)
-        self:SetRandomDestination(true)
+        self:SetRandomDestination(not self:GetData("float"))
+        
         TARDIS:Message(ply, "Random destination has been selected")
     end,
     serveronly=true,
