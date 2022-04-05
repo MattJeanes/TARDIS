6898e7163665571f875f23092b3ed32429abcc1f
Artiom Parygin <artiom.par@ya.ru>
2022-04-05 13:09:34 +0300

    Power dependency

--- lua/entities/gmod_tardis_interior/modules/sh_interior_lights.lua
+++ lua/entities/gmod_tardis_interior/modules/sh_interior_lights.lua
@@ -77,52 +77,65 @@ else -- SERVER
             self.lamps = {}
 
             for k,v in pairs(lamps) do
-                local lamp = MakeLamp(nil, --self:GetCreator(),
-                    v.color.r, v.color.g, v.color.b, KEY_NONE, true,
-                    v.texture or "effects/flashlight/soft",
-                    "models/maxofs2d/lamp_projector.mdl",
-                    v.fov or 90, v.distance or 1024, v.brightness or 3.0, true,
-                    {
-                        Pos = self:LocalToWorld(v.pos or Vector(0,0,0)),
-                        Angle = v.ang or Angle(0,0,0),
-                    })
+                if v then
+                    local lamp = MakeLamp(nil, --self:GetCreator(),
+                        v.color.r, v.color.g, v.color.b, KEY_NONE, true,
+                        v.texture or "effects/flashlight/soft",
+                        "models/maxofs2d/lamp_projector.mdl",
+                        v.fov or 90, v.distance or 1024, v.brightness or 3.0, true,
+                        {
+                            Pos = self:LocalToWorld(v.pos or Vector(0,0,0)),
+                            Angle = v.ang or Angle(0,0,0),
+                        })
 
-                lamp:SetUnFreezable(false)
-                lamp:GetPhysicsObject():EnableGravity(false)
-                lamp:GetPhysicsObject():EnableMotion(false)
+                    lamp:SetUnFreezable(false)
+                    lamp:GetPhysicsObject():EnableGravity(false)
+                    lamp:GetPhysicsObject():EnableMotion(false)
 
-                if self.metadata.Interior.LampsDebug == true then
-                    lamp:SetUseType(SIMPLE_USE)
-                    lamp.Use = function(ply)
-                        local clr = lamp:GetColor()
-                        local pos = self:WorldToLocal(lamp:GetPos())
-                        local ang = lamp:GetAngles()
-                        print("{\n\tcolor = Color(" .. clr.r .. ", " .. clr.g .. ", " .. clr.b .. "),")
-                        print("\ttexture = \"" .. lamp:GetFlashlightTexture() .. "\",")
-                        print("\tfov = " .. lamp:GetLightFOV() .. ",")
-                        print("\tdistance = " .. lamp:GetDistance() .. ",")
-                        print("\tbrightness = " .. lamp:GetBrightness() .. ",")
-                        print("\tpos = Vector(" .. pos.x .. ", " .. pos.y .. ", " .. pos.z .. "),")
-                        print("\tang = Angle(" .. ang.x .. ", " .. ang.y .. ", " .. ang.z .. "),")
-                        print("},")
+                    if self.metadata.Interior.LampsDebug == true then
+                        lamp:SetUseType(SIMPLE_USE)
+                        lamp.Use = function(ply)
+                            local clr = lamp:GetColor()
+                            local pos = self:WorldToLocal(lamp:GetPos())
+                            local ang = lamp:GetAngles()
+                            print("{\n\tcolor = Color(" .. clr.r .. ", " .. clr.g .. ", " .. clr.b .. "),")
+                            print("\ttexture = \"" .. lamp:GetFlashlightTexture() .. "\",")
+                            print("\tfov = " .. lamp:GetLightFOV() .. ",")
+                            print("\tdistance = " .. lamp:GetDistance() .. ",")
+                            print("\tbrightness = " .. lamp:GetBrightness() .. ",")
+                            print("\tpos = Vector(" .. pos.x .. ", " .. pos.y .. ", " .. pos.z .. "),")
+                            print("\tang = Angle(" .. ang.x .. ", " .. ang.y .. ", " .. ang.z .. "),")
+                            print("},")
+                        end
+                        lamp:SetCollisionGroup(COLLISION_GROUP_WORLD)
+                    else
+                        lamp:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
+                        lamp:SetRenderMode(RENDERMODE_TRANSALPHA)
+                        lamp:SetColor(Color(v.color.r, v.color.g, v.color.b,0))
                     end
-                    lamp:SetCollisionGroup(COLLISION_GROUP_WORLD)
-                else
-                    lamp:SetCollisionGroup(COLLISION_GROUP_IN_VEHICLE)
-                    lamp:SetRenderMode(RENDERMODE_TRANSALPHA)
-                    lamp:SetColor(Color(v.color.r, v.color.g, v.color.b,0))
-                end
 
-                table.insert(self.lamps, lamp)
+                    lamp.lamp_data = v
+
+                    table.insert(self.lamps, lamp)
+                end
             end
         end)
     end
 
-    ENT:AddHook("OnRemove", "music", function(self)
+    ENT:AddHook("OnRemove", "lamps", function(self)
         if not self.lamps then return end
         for k,v in pairs(self.lamps) do
             if IsValid(v) then
                 v:Remove()
+            end
+        end
+    end)
+
+    ENT:AddHook("PowerToggled", "lamps", function(self, on)
+        if not self.lamps then return end
+        for k,v in pairs(self.lamps) do
+            if IsValid(v) and v.lamp_data.nopower ~= true then
+                v:SetOn(on)
             end
         end
     end)
