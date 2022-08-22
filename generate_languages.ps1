7fd0f5a40e05778d37b4c774552b95c7195f90bf
Matt Jeanes <mattjeanes23@gmail.com>
2022-08-22 02:27:28 +0100

    auto generate translations from source files managed by crowdin

---
+++ generate_languages.ps1
@@ -0,0 +1,28 @@
+$sourceLanguageFolder = Join-Path $PSScriptRoot "i18n/languages"
+$targetLanguageFolder = Join-Path $PSScriptRoot "lua/tardis/languages"
+
+Get-ChildItem $sourceLanguageFolder | ForEach-Object {
+    $code = $_.BaseName
+    $language = Get-Content -Raw $_.FullName | ConvertFrom-Json -AsHashtable
+    $targetFilename = Join-Path $targetLanguageFolder "$($code.ToLower()).lua"
+
+    $content = "-- AUTO GENERATED FILE - DO NOT EDIT --`n"
+    $content += "-- SOURCE FILE: $($_.FullName.Replace($PSScriptRoot, [string]::Empty).Substring(1)) --`n`n"
+    $content += "local T = {}`n"
+    $content += "T.Code = `"$code`"`n"
+    $content += "T.Name = `"$($language.Name)`"`n"
+    $content += "T.Phrases = {`n"
+
+    $language.Phrases.Keys | Sort-Object | ForEach-Object {
+        $key = $_
+        $phrase = $language.Phrases[$key]
+        $phrase = $phrase.Replace("`n", "\n")
+        $phrase = $phrase.Replace("`"", "\`"")
+        $content += "    [`"$key`"] = `"$phrase`",`n"
+    }
+
+    $content += "}`n`n"
+    $content += "TARDIS:AddLanguage(T)`n"
+
+    Set-Content -NoNewline -Path $targetFilename -Value $content
+}
