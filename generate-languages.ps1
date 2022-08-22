be956b29b59c9c5a86f42d8f3da59529de895d5f
Matt Jeanes <mattjeanes23@gmail.com>
2022-08-22 03:45:31 +0100

    don't generate language for empty language

--- generate-languages.ps1
+++ generate-languages.ps1
@@ -1,9 +1,14 @@
 $sourceLanguageFolder = Join-Path $PSScriptRoot "i18n/languages"
 $targetLanguageFolder = Join-Path $PSScriptRoot "lua/tardis/languages"
 
+$sourceLanguageCode = "en"
+$sourceLanguage = Get-Content -Raw (Join-Path $sourceLanguageFolder "$sourceLanguageCode.json") | ConvertFrom-Json -AsHashtable
+$targetLanguageFolder | Get-ChildItem | Remove-Item
+
 Get-ChildItem $sourceLanguageFolder | ForEach-Object {
     $code = $_.BaseName
-    $language = Get-Content -Raw $_.FullName | ConvertFrom-Json -AsHashtable
+    $languageFile = Get-Content -Raw $_.FullName
+    $language = $languageFile | ConvertFrom-Json -AsHashtable
     $targetFilename = Join-Path $targetLanguageFolder "$($code.ToLower()).lua"
 
     $content = "-- AUTO GENERATED FILE - DO NOT EDIT --`n"
@@ -13,12 +18,21 @@ Get-ChildItem $sourceLanguageFolder | ForEach-Object {
     $content += "T.Name = `"$($language.Name)`"`n"
     $content += "T.Phrases = {`n"
 
+    $changes = $false
     $language.Phrases.Keys | Sort-Object | ForEach-Object {
         $key = $_
         $phrase = $language.Phrases[$key]
+        if (-not $changes -and $sourceLanguage.Phrases[$key] -ne $phrase) {
+            $changes = $true
+        }
         $phrase = $phrase.Replace("`n", "\n")
         $phrase = $phrase.Replace("`"", "\`"")
         $content += "    [`"$key`"] = `"$phrase`",`n"
+    }
+
+    if (-not $changes -and $sourceLanguageCode -ne $code) {
+        Write-Warning "No phrases changed in $code compared to $sourceLanguageCode baseline, skipping.."
+        return
     }
 
     $content += "}`n`n"
