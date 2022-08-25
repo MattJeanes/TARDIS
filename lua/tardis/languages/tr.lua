f19b8910c705e86503ad2b7d1ff8e046fe934b68
MattJeanes <MattJeanes@users.noreply.github.com>
2022-08-25 00:20:28 +0000

    Update Generated Languages

---
+++ lua/tardis/languages/tr.lua
@@ -0,0 +1,11 @@
+-- AUTO GENERATED FILE - DO NOT EDIT --
+-- SOURCE FILE: i18n/languages/tr.json --
+
+local T = {}
+T.Code = "tr"
+T.Name = "Türkçe"
+T.Phrases = {
+    ["Binds.AnyKey"] = "Binds",
+}
+
+TARDIS:AddLanguage(T)
