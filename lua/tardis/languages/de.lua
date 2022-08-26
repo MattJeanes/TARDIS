83ab9d1d221383746dd7862cfc8bce1a725c7f00
MattJeanes <MattJeanes@users.noreply.github.com>
2022-08-26 22:43:02 +0000

    Update Generated Languages

--- lua/tardis/languages/de.lua
+++ lua/tardis/languages/de.lua
@@ -12,6 +12,15 @@ T.Phrases = {
     ["Binds.Sections.Destination.Boost"] = "Schub",
     ["Binds.Sections.Destination.Boost.Description"] = "Halten Sie diese Taste beim Navigieren gedrückt, um die Geschwindigkeit zu erhöhen",
     ["Binds.Sections.Destination.Down"] = "Runter",
+    ["Binds.Sections.Destination.Forward"] = "Vorwärts",
+    ["Binds.Sections.Destination.Left"] = "Links",
+    ["Binds.Sections.Destination.Right"] = "Recht",
+    ["Binds.Sections.Destination.Rotate"] = "Rotieren",
+    ["Binds.Sections.Destination.Rotate.Description"] = "Halten Sie dies mit der Boost-Taste und der linken oder rechten Taste, um es zu drehen",
+    ["Binds.Sections.Destination.Slow"] = "Langsam",
+    ["Binds.Sections.Destination.Slow.Description"] = "Halten Sie diese Taste gedrückt, um die Bewegung zu verlangsamen",
+    ["Binds.Sections.Destination.SnapToFloor"] = "Einrasten am Boden",
+    ["Binds.Sections.Destination.SnapToFloor.Description"] = "Drücken Sie diese Taste, um die nächstgelegene Etage anzufahren",
 }
 
 TARDIS:AddLanguage(T)
