aefc3a2cb4b74a9ea113a289c1763ed1247c0593
MattJeanes <MattJeanes@users.noreply.github.com>
2022-08-26 17:55:27 +0000

    Update Generated Languages

---
+++ lua/tardis/languages/cy.lua
@@ -0,0 +1,30 @@
+-- AUTO GENERATED FILE - DO NOT EDIT --
+-- SOURCE FILE: i18n/languages/cy.json --
+
+local T = {}
+T.Code = "cy"
+T.Name = "Saesneg"
+T.Phrases = {
+    ["Binds.AnyKey"] = "Gwasgwch ynrhyw cywair",
+    ["Binds.Invalid"] = "Annilys",
+    ["Binds.Key"] = "Allwedd",
+    ["Binds.Sections.Destination"] = "Cyrchfan",
+    ["Binds.Sections.Destination.Backward"] = "Tu ôl",
+    ["Binds.Sections.Destination.Boost"] = "Cynyddu cyflymder",
+    ["Binds.Sections.Destination.Boost.Description"] = "Gwasgwch y cywair hwn pan mordwyo i gyflymu",
+    ["Binds.Sections.Destination.Down"] = "i lawr",
+    ["Binds.Sections.Destination.Forward"] = "Ymlaen",
+    ["Binds.Sections.Destination.Left"] = "i'r chwith",
+    ["Binds.Sections.Destination.Right"] = "i'r dde",
+    ["Binds.Sections.Destination.Rotate"] = "Cylchdroi",
+    ["Binds.Sections.Destination.Rotate.Description"] = "Gwasgwch hyn gyda'r cynyddu cyflymder a'r cywair chwith neu dde i cylchdroi",
+    ["Binds.Sections.Destination.Slow"] = "Araf",
+    ["Binds.Sections.Destination.Slow.Description"] = "Gwasgwch y cywair hwn i arafu eich symudiad",
+    ["Binds.Sections.Destination.SnapToFloor"] = "Chlecia i'r llawr",
+    ["Binds.Sections.Destination.SnapToFloor.Description"] = "Gwasgwch y cywair hwn i chlecia i'r llawr agosach",
+    ["Binds.Sections.Destination.Up"] = "i fyny",
+    ["Binds.Sections.Flight.Backward"] = "Yn ôl",
+    ["Binds.Sections.Flight.Boost.Description"] = "Dal y cywair hwn pan hedfan i gyflymu",
+}
+
+TARDIS:AddLanguage(T)
