c4f1cfeebea5cf4d9f52c4ddc726bc60dcc756c5
Matt Jeanes <mattjeanes23@gmail.com>
2022-08-28 18:31:09 +0100

    animate helmic regulator and atom accel as per legacy

---
+++ lua/entities/gmod_tardis/modules/sh_spin.lua
@@ -0,0 +1,75 @@
+-- Spin
+
+function ENT:GetSpinDir()
+    return self:GetData("spindir", -1)
+end
+
+function ENT:GetSpinDirText(show_next)
+    local current = self:GetData("spindir", -1)
+    if show_next == true then
+        current = self:GetData("spindir_prev", 0)
+    end
+
+    local text
+    if current == -1 then
+        text = "Spin.Directions.AntiClockwise"
+    elseif current == 0 then
+        text = "Spin.Directions.None"
+    elseif current == 1 then
+        text = "Spin.Directions.Clockwise"
+    end
+
+    return text
+end
+
+if SERVER then
+    ENT:AddHook("Initialize", "spin", function(self)
+        self:SetData("spindir", -1, true)
+        self:SetData("spindir_prev", 0, true)
+    end)
+
+    function ENT:ToggleSpin()
+        local current = self:GetData("spindir", -1)
+        local prev = self:GetData("spindir_prev", 0)
+
+        self:SetData("spindir_prev", current, true)
+        self:SetData("spindir", prev, true)
+    end
+
+    function ENT:CycleSpinDir()
+        local current = self:GetData("spindir", -1)
+        local prev = self:GetData("spindir_prev", 0)
+
+        self:SetData("spindir_prev", current, true)
+        self:SetData("spindir", -prev, true)
+    end
+
+    function ENT:SwitchSpinDir()
+        local current = self:GetData("spindir", -1)
+        local prev = self:GetData("spindir_prev", 0)
+
+        self:SetData("spindir_prev", -prev, true)
+        self:SetData("spindir", -current, true)
+    end
+
+    function ENT:SetSpinDir(dir)
+        return self:SetData("spindir", dir, true)
+    end
+
+    ENT:AddHook("ToggleDoorReal", "spin-dir", function(self,open)
+        if TARDIS:GetSetting("opened-door-no-spin", self) then
+            local current = self:GetData("spindir", -1)
+            local before = self:GetData("spindir_before_door", nil)
+    
+            if open and self:GetSpinDir() ~= 0 then
+                self:SetData("spindir_before_door", current, true)
+                self:SetData("spindir_prev", current, true)
+                self:SetData("spindir", 0, true)
+            elseif not open and self:GetSpinDir() == 0 and before ~= nil then
+                self:SetData("spindir_before_door", nil, true)
+                self:SetData("spindir_prev", current, true)
+                self:SetData("spindir", before, true)
+            end
+        end
+    end)
+end
--- lua/tardis/parts/default/default_int_atomaccel.lua
+++ lua/tardis/parts/default/default_int_atomaccel.lua
@@ -8,4 +8,27 @@ PART.Model = "models/drmatt/tardis/atomaccel.mdl"
 PART.AutoSetup = true
 PART.Collision = true
 
+if CLIENT then
+    function PART:Think()
+        if not self.exterior:GetData("flight") then return end
+        local spindir = self.exterior:GetSpinDir()
+        if spindir == 0 then return end
+        local pos = self:GetPoseParameter("switch")
+        local target
+        if spindir == 1 then
+            target = 0
+            if pos == 0 then
+                pos = 1
+            end
+        else
+            target = 1
+            if pos == 1 then
+                pos = 0
+            end
+        end
+        self:SetPoseParameter("switch", math.Approach( pos, target, FrameTime() * 1 ))
+        self:InvalidateBoneCache()
+    end
+end
+
 TARDIS:AddPart(PART)
