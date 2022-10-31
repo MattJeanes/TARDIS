265b9f8e63202a7bcc5c1be6fbd8d60af91f4b01
MattJeanes <MattJeanes@users.noreply.github.com>
2022-10-31 17:57:07 +0000

    Update Generated Languages

---
+++ lua/tardis/languages/pt-pt.lua
@@ -0,0 +1,22 @@
+-- AUTO GENERATED FILE - DO NOT EDIT --
+-- SOURCE FILE: i18n/languages/pt-PT.json --
+
+local T = {}
+T.Code = "pt-PT"
+T.Name = "Português"
+T.Phrases = {
+    ["Binds.AnyKey"] = "Aperte qualquer tecla",
+    ["Binds.Invalid"] = "Inválido",
+    ["Binds.Key"] = "Tecla",
+    ["Binds.Sections.Destination.Boost.Description"] = "Segure esta tecla enquanto estiver navegando para acelerar",
+    ["Binds.Sections.Destination.Rotate.Description"] = "Mantenha esta tecla pressionada com a de aceleração e use a tecla esquerda ou direita para girar",
+    ["Binds.Sections.Destination.Slow.Description"] = "Segure esta tecla para desacelerar o movimento",
+    ["Binds.Sections.Destination.SnapToFloor"] = "Grudar ao chão",
+    ["Binds.Sections.Destination.SnapToFloor.Description"] = "Pressione esta tecla para grudar ao chão mais próximo",
+    ["Binds.Sections.Flight.Boost.Description"] = "Segure esta tecla enquanto voa para acelerar",
+    ["Binds.Sections.Flight.Rotate.Description"] = "Segure esta tecla enquanto usa esquerda e direita para girar",
+    ["Binds.Sections.Flight.SpinDirection"] = "Direção da Rotação",
+    ["Binds.Sections.Flight.SpinDirection.Description"] = "Alterna a direção em que a TARDIS gira enquanto voa",
+}
+
+TARDIS:AddLanguage(T)
