b2028678906f00769315f5fb15eb6ac0f66d63ac
BriannBanned <bthegoldenfreddy@gmail.com>
2022-11-14 05:10:38 -0700

    Changed how artron energy is changed.

--- lua/entities/gmod_tardis/modules/sh_artronenergy.lua
+++ lua/entities/gmod_tardis/modules/sh_artronenergy.lua
@@ -22,159 +22,158 @@ TARDIS:AddSetting({
     section=SETTING_SECTION,
     name="MaxHealth",
 })
+
 if SERVER then
-
-local function passiveArtronPower(this)
-
-    timer.Simple( 5, function() 
-        if not IsValid(this.Entity) then 
-            return 
+    function ENT:ChangeArtron(setartron, add) -- Second one is for if you wanna add or set it.
+        local currentArtron = self:GetData("artron-val")
+        local maxArtron = GetConVar("te_maxartronenergy"):GetInt()
+        if currentArtron == nil then  
+            currentArtron = 0
         end
-        if not this:GetData("vortex") and not this:GetData("teleport") and not this:GetData("flight") and this:GetData("power-state") and not this:GetData("cloak") then 
+        if currentArtron + setartron > maxArtron then
+            if setartron == maxArtron then
+                return
+            end
+            currentArtron = 0
+            setartron = maxArtron
+
+        end  
+        if currentArtron + setartron < 0 then
+            if setartron == 0 then
+                return
+            end
+            currentArtron = 0
+            setartron = 0
+
+        end  
+        if add == false or add == nil then
+            currentArtron = 0
+        end
+        self:SetData("artron-val", setartron + currentArtron, true)
+    end
+
+    local function passiveArtronPower(this)
+
+        timer.Simple( 5, function() 
+            if not IsValid(this.Entity) then 
+                return 
+            end
+            if not this:GetData("vortex") and not this:GetData("teleport") and not this:GetData("flight") and this:GetData("power-state") and not this:GetData("cloak") then 
+                this:ChangeArtron(50, true)
+            end
+            passiveArtronPower(this)
+        end )
+
+    end
+
+    local function ArtronPowerOff(this)
+
+        timer.Simple( 5, function() 
+            if not IsValid(this.Entity) or this:GetData("power-state") then 
+                return 
+            end
             if this:GetData("artron-val") < GetConVar("te_maxartronenergy"):GetInt() then
-                this:SetData("artron-val", this:GetData("artron-val") + 50, true)
+                this:ChangeArtron(250, true)
             end
-            if this:GetData("artron-val") > GetConVar("te_maxartronenergy"):GetInt() then
-                this:SetData("artron-val", GetConVar("te_maxartronenergy"):GetInt())
+            ArtronPowerOff(this)
+            checkTardisArtron(this)
+
+        end )
+
+    end
+
+    local function DematleechLoop(this)
+        timer.Simple( 1, function() 
+            if not IsValid(this.Entity) or not this:GetData("vortex") or not this:GetData("teleport") then 
+                return 
+            end
+            this:ChangeArtron(-24, true)
+            DematleechLoop(this)
+        end )
+    end
+
+    local function FlyingLeechLoop(this)
+        timer.Simple( 1, function() 
+            if not IsValid(this.Entity) or not this:GetData("flight") then 
+                return 
+            end
+            this:ChangeArtron(-25, true)
+            FlyingLeechLoop(this)
+        end )
+    end
+
+    local function CloakLeechLoop(this)
+        timer.Simple( 1, function() 
+            if not IsValid(this.Entity) or not this:GetData("cloak") then 
+                return 
+            end
+            this:ChangeArtron(-20, true)
+            CloakLeechLoop(this)
+        end )
+    end
+
+
+    ENT:AddHook("Initialize","artron-init",function(self)
+        if GetConVar("te_startwithmaxenergy"):GetBool() == true then
+            self:ChangeArtron(GetConVar("te_maxartronenergy"):GetInt(), false)
+        else
+            self:ChangeArtron(500, false)
+        end
+        passiveArtronPower(self)
+
+    end)
+
+
+    ENT:AddHook("CanTurnOnFloat", "floatartron", function(self)
+        self:ChangeArtron(-50, true)
+    end)
+
+    ENT:AddHook("CanDemat", "dematartron", function(self)
+        self:ChangeArtron(-500, true)
+    end)
+
+    ENT:AddHook("CanTogglePower", "artronpowertoggled", function(self, on)
+        if self:GetData("artron-val") <= 0 and self:GetData("power-state") == false then
+            return false
+        end
+    end)
+
+    ENT:AddHook("PowerToggled", "artronpowertoggledafter", function(self, on)
+        if self:GetData("power-state") == false then
+            ArtronPowerOff(self)
+        end
+    end)
+
+    ENT:AddHook("StopDemat", "artronpowervortexleech", function(self, on)
+        DematleechLoop(self)
+    end)
+
+    ENT:AddHook("CanTurnOnFlight", "artronpowerflightleech", function(self, on)
+        FlyingLeechLoop(self)
+    end)
+
+    ENT:AddHook("CanToggleCloak", "artronpowercloakleech", function(self, on)
+        CloakLeechLoop(self)
+    end)
+
+
+
+
+    ENT:AddHook("Think", "powerdisableartron", function(self)
+        if  self:GetData("power-state") == true and self:GetData("artron-val") <= 0 then
+            if self:GetData("cloak") == true then 
+                self:SetCloak(false)
+            end
+            if self:GetData("flight") == true then
+                self:SetFlight(false)
+            end
+            self:SetPower(false)
+            self:SetHandbrake(true)
+            self:SetHandbrake(false)
+            for k,_ in pairs(self.occupants) do
+                TARDIS:ErrorMessage(k, "Artron Depleted.")
             end
         end
-        passiveArtronPower(this)
-        checkTardisArtron(this)
-    end )
-
-end
-
-local function ArtronPowerOff(this)
-
-    timer.Simple( 5, function() 
-        if not IsValid(this.Entity) or this:GetData("power-state") then 
-            return 
-        end
-        if this:GetData("artron-val") < GetConVar("te_maxartronenergy"):GetInt() then
-            this:SetData("artron-val", this:GetData("artron-val") + 250, true)
-        end
-        ArtronPowerOff(this)
-        checkTardisArtron(this)
-
-    end )
-
-end
-
-local function DematleechLoop(this)
-    timer.Simple( 1, function() 
-        if not IsValid(this.Entity) or not this:GetData("vortex") or not this:GetData("teleport") then 
-            return 
-        end
-        this:SetData("artron-val", this:GetData("artron-val") - 24, true)
-        if this:GetData("artron-val") < 0 then
-            this:SetData("artron-val", 0)
-        end
-        DematleechLoop(this)
-    end )
-end
-
-local function FlyingLeechLoop(this)
-    timer.Simple( 1, function() 
-        if not IsValid(this.Entity) or not this:GetData("flight") then 
-            return 
-        end
-        this:SetData("artron-val", this:GetData("artron-val") - 25, true)
-        if this:GetData("artron-val") < 0 then
-            this:SetData("artron-val", 0)
-        end
-        FlyingLeechLoop(this)
-    end )
-end
-
-local function CloakLeechLoop(this)
-    timer.Simple( 1, function() 
-        if not IsValid(this.Entity) or not this:GetData("cloak") then 
-            return 
-        end
-        this:SetData("artron-val", this:GetData("artron-val") - 25, true)
-        if this:GetData("artron-val") < 0 then
-            this:SetData("artron-val", 0)
-        end
-        CloakLeechLoop(this)
-    end )
-end
-
-
-ENT:AddHook("Initialize","artron-init",function(self)
-    if GetConVar("te_startwithmaxenergy"):GetBool() == true then
-        self:SetData("artron-val", GetConVar("te_maxartronenergy"):GetInt(), true)
-    else
-        self:SetData("artron-val", 500, true)
-    end
-    passiveArtronPower(self)
-
-end)
-
-
-ENT:AddHook("CanTurnOnFloat", "floatartron", function(self)
-    self:SetData("artron-val", self:GetData("artron-val") - 50, true)
-    checkTardisArtron(self)
-end)
-
-ENT:AddHook("CanDemat", "dematartron", function(self)
-    self:SetData("artron-val", self:GetData("artron-val") - 500, true)
-    checkTardisArtron(self)
-    if self:GetData("artron-val") <= 0 then
-        return false
-    end
-end)
-
-ENT:AddHook("CanTogglePower", "artronpowertoggled", function(self, on)
-    if self:GetData("artron-val") <= 0 and self:GetData("power-state") == false then
-        return false
-    end
-end)
-
-ENT:AddHook("PowerToggled", "artronpowertoggledafter", function(self, on)
-    if self:GetData("power-state") == false then
-        ArtronPowerOff(self)
-    end
-end)
-
-ENT:AddHook("StopDemat", "artronpowervortexleech", function(self, on)
-    DematleechLoop(self)
-end)
-
-ENT:AddHook("CanTurnOnFlight", "artronpowerflightleech", function(self, on)
-    FlyingLeechLoop(self)
-end)
-
-ENT:AddHook("CanToggleCloak", "artronpowercloakleech", function(self, on)
-    CloakLeechLoop(self)
-end)
-
-function checkTardisArtron(this)
-    if this:GetData("artron-val") < 0 then
-        this:SetData("artron-val", 0, true)
-    elseif this:GetData("artron-val") > GetConVar("te_maxartronenergy"):GetInt() then
-                
-        this:SetData("artron-val", GetConVar("te_maxartronenergy"):GetInt(), true)
- 
-    end
-end
-
-
-ENT:AddHook("Think", "powerdisableartron", function(self)
-    if  self:GetData("power-state") == true and self:GetData("artron-val") <= 0 then
-        if self:GetData("cloak") == true then 
-            self:SetCloak(false)
-        end
-        if self:GetData("flight") == true then
-            self:SetFlight(false)
-        end
-        self:SetPower(false)
-        self:SetHandbrake(true)
-        self:SetHandbrake(false)
-        for k,_ in pairs(self.occupants) do
-            TARDIS:ErrorMessage(k, "Artron Depleted.")
-        end
-    end
-end)
-
+    end)
 end
 
