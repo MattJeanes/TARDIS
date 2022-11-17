aef65bf251ea1b70bbb1dbe50f06b24be016addd
Matt Jeanes <mattjeanes23@gmail.com>
2022-11-17 20:47:39 +0000

    revert draw fix for now until we run into it again

--- lua/tardis/sh_parts.lua
+++ lua/tardis/sh_parts.lua
@@ -28,9 +28,7 @@ function TARDIS.DrawOverride(self,override)
             if self.PreDraw then self:PreDraw() end
             if self.UseTransparencyFix and (not override) then
                 render.SetBlend(0)
-                if self.o.Draw ~= TARDIS.DrawOverride then
-                    self.o.Draw(self)
-                end
+                self.o.Draw(self)
                 render.SetBlend(1)
             else
                 self.o.Draw(self)
