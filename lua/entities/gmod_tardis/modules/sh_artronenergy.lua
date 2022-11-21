bc0529389a46645595083e52043bf131f24cab8d
Artiom Parygin <artiom.par@ya.ru>
2022-11-21 22:28:38 +0300

    Better artron energy timer

--- lua/entities/gmod_tardis/modules/sh_artronenergy.lua
+++ lua/entities/gmod_tardis/modules/sh_artronenergy.lua
@@ -1,4 +1,24 @@
 if SERVER then
+
+    local artron_values = {
+        initial_value = 100,
+
+        cost_demat = -500,
+        cost_enable_float = -50,
+        cost_disable_float = -25,
+
+        -- every 1 second:
+        spend_vortex_teleport = -24,
+        spend_flight = -25,
+        spend_cloak = -20,
+
+        -- every 5 seconds:
+        charge_handbrake = 400,
+        charge_poweroff = 250,
+        charge_normal = 50,
+        charge_float = 20,
+    }
+
     function ENT:SetArtron(value)
         local maxArtron = TARDIS:GetSetting("artron_energy_max")
         value = math.max(0, math.min(value, maxArtron))
@@ -6,6 +26,7 @@ if SERVER then
         if value == 0 then
             self:CallHook("ArtronDepleted")
         end
+        tardisdebug("ARTRON: ", value)
     end
 
     function ENT:AddArtron(value)
@@ -13,127 +34,105 @@ if SERVER then
         self:SetArtron(currentArtron + value)
     end
 
-    ENT:AddHook("Initialize", "artron_energy", function(self)
-        local artron = 100
+    ENT:AddHook("Initialize", "artron", function(self)
+        local artron = artron_values.initial_value
         if TARDIS:GetSetting("artron_energy_start_full") == true then
             self:SetArtron(TARDIS:GetSetting("artron_energy_max"))
         end
         self:SetArtron(artron)
-        passiveArtronPower(self)
     end)
 
     ENT:AddHook("ArtronDepleted", "poweroff", function(self)
         if self:GetPower() then
-            if self:GetData("cloak") == true then 
-                self:SetCloak(false)
-            end
-            if self:GetData("floatfirst") == true then
-                self:ToggleFloat()
-            end
-            if self:GetData("flight") == true then
-                self:SetFlight(false)
-            end
-
-            self:SetPower(false) -- Do all flight and cloak stuff before this because it wont work i think i dont remember -\_(o.o)_/-
-            self:SetHandbrake(true)
-            self:SetHandbrake(false)
+            self:SetPower(false)
             for k,_ in pairs(self.occupants) do
                 TARDIS:ErrorMessage(k, "Artron Depleted.")
             end
         end
     end)
 
-
-
-
-
-    local function passiveArtronPower(this)
-
-        timer.Simple(5, function() 
-            if not IsValid(this.Entity) then return end
-            local vortex = this:GetData("vortex")
-            local teleport = this:GetData("teleport")
-            local flight = this:GetData("flight")
-            local power = this:GetData("power-state")
-            local cloak = this:GetData("cloak")
-            if not vortex and not teleport and not flight and power and not cloak then 
-                this:AddArtron(50)
-            end
-            passiveArtronPower(this)
-        end)
-    end
-
-    local function ArtronPowerOff(this)
-        timer.Simple(5, function() 
-            if not IsValid(this.Entity) or this:GetPower() then return end
-            if this:GetData("artron-val") < TARDIS:GetSetting("artron_energy_max") then
-                this:AddArtron(250)
-            end
-            ArtronPowerOff(this)
-        end)
-    end
-
-    local function DematleechLoop(this)
-        timer.Simple( 1, function()
-            if not IsValid(this.Entity) or not this:GetData("vortex") or not this:GetData("teleport") then 
-                return
-            end
-            this:AddArtron(-24)
-            DematleechLoop(this)
-        end )
-    end
-
-    local function FlyingLeechLoop(this)
-        timer.Simple( 1, function() 
-            if not IsValid(this.Entity) or not this:GetData("flight") then 
-                return 
-            end
-            this:AddArtron(-25)
-            FlyingLeechLoop(this)
-        end )
-    end
-
-    local function CloakLeechLoop(this)
-        timer.Simple( 1, function() 
-            if not IsValid(this.Entity) or not this:GetData("cloak") then 
-                return 
-            end
-            this:AddArtron(-20)
-            CloakLeechLoop(this)
-        end )
-    end
-
-    ENT:AddHook("CanTurnOnFloat", "floatartron", function(self)
-        self:AddArtron(-50)
+    ENT:AddHook("ArtronDepleted", "teleport", function(self)
+        if self:GetData("teleport") or self:GetData("vortex") then
+            self:InterruptTeleport()
+        end
     end)
 
-    ENT:AddHook("DematStart", "dematartron", function(self)
-        self:AddArtron(-500)
+    ENT:AddHook("ArtronDepleted", "cloak", function(self)
+        if self:GetCloak() then
+            self:SetCloak(false)
+        end
     end)
 
-    ENT:AddHook("CanTogglePower", "artronpowertoggled", function(self, on)
-        if self:GetData("artron-val") <= 0 and self:GetData("power-state") == false then
+    ENT:AddHook("Think", "artron", function(self)
+        if not TARDIS:GetSetting("artron_energy") then return false end
+
+        -- if artron energy should decrease, it happens every second
+        if CurTime() < self:GetData("artron_next_decrease_time", 0) then return end
+        self:SetData("artron_next_decrease_time", CurTime() + 1)
+
+        local power = self:GetPower()
+        local vortex = self:GetData("vortex")
+        local teleport = self:GetData("teleport")
+        local flight = self:GetData("flight")
+        local handbrake = self:GetData("handbrake")
+        local cloak = self:GetCloak()
+        local float = self:GetData("floatfirst")
+
+        local change = 0
+
+        if vortex or teleport then
+            change = change + artron_values.spend_vortex_teleport
+        end
+        if flight then
+            change = change + artron_values.spend_flight
+        end
+        if cloak then
+            change = change + artron_values.spend_cloak
+        end
+
+        if change < 0 then
+            self:AddArtron(change)
+            return
+        end
+
+        -- if artron energy is charging, it happens every 5 seconds
+        if CurTime() < self:GetData("artron_next_charge_time", 0) then return end
+        self:SetData("artron_next_charge_time", CurTime() + 5)
+
+        if handbrake then
+            self:AddArtron(artron_values.charge_handbrake)
+            return
+        end
+        if not power then
+            self:AddArtron(artron_values.charge_poweroff)
+            return
+        end
+        if float then
+            self:AddArtron(artron_values.change_float)
+            return
+        end
+
+        self:AddArtron(artron_values.charge_normal) -- default state
+    end)
+
+    ENT:AddHook("FloatToggled", "artron", function(self, on)
+        if on then
+            self:AddArtron(artron_values.cost_enable_float)
+        else
+            self:AddArtron(artron_values.cost_disable_float)
+        end
+    end)
+
+    ENT:AddHook("DematStart", "artron", function(self)
+        self:AddArtron(artron_values.cost_demat)
+    end)
+
+    ENT:AddHook("CanTogglePower", "artron", function(self, on)
+        if self:GetData("artron-val") <= 0 and self:GetPower() == false then
             return false
         end
     end)
 
-    ENT:AddHook("PowerToggled", "artronpowertoggledafter", function(self, on)
-        if on == false then
-            ArtronPowerOff(self)
-        end
-    end)
-
-    ENT:AddHook("StopDemat", "artronpowervortexleech", function(self, on)
-        DematleechLoop(self)
-    end)
-
-    ENT:AddHook("CanTurnOnFlight", "artronpowerflightleech", function(self, on)
-        FlyingLeechLoop(self)
-    end)
-
-    ENT:AddHook("CanToggleCloak", "artronpowercloakleech", function(self, on)
-        CloakLeechLoop(self)
-    end)
 else
 
     hook.Add("HUDPaint", "TARDIS-HUD2", function()
@@ -143,6 +142,7 @@ else
         if not IsValid(tardis) then return end
         local sw = ScrW()
         local sh = ScrH()
+
         local val = tardis:GetData("artron-val", 0)
         local percent = val * 100 / TARDIS:GetSetting("artron_energy_max")
         local health = math.ceil(percent)
@@ -154,27 +154,19 @@ else
         local healthfont = (height == 120) and "TARDIS-HUD-Large" or "TARDIS-HUD-Med"
         local x = (ScrW()-width)*0.02
         local y = (ScrH()-height)*0.025
+
         draw.RoundedBox( 10, x + 190, y, width, height, NamedColor("BgColor") )
-        local textcolor
+
         local textcolor = (health > 0) and NamedColor("FgColor") or NamedColor("Caution")
-        if (health > 20) then textcolor = NamedColor("FgColor")
-        else textcolor = NamedColor("Caution") end
+        if (health > 20) then
+            textcolor = NamedColor("FgColor")
+        else
+            textcolor = NamedColor("Caution")
+        end
+
         draw.DrawText( TARDIS:GetPhrase("ARTRON"), "TARDIS-HUD-Small", x+200, y+10, textcolor, TEXT_ALIGN_LEFT )
         draw.DrawText( tostring(health) .. "%", healthfont, x+200, y+45, textcolor, TEXT_ALIGN_LEFT )
     end)
-
-    list.Set("DesktopWindows", "TardisHUD2", {
-        title = "TARDIS",
-        icon = "materials/vgui/tardis_context_menu.png",
-        init = function()
-            local ext = LocalPlayer():GetTardisData("exterior")
-            if IsValid(ext) then
-                TARDIS:HUDScreen()
-            else
-                TARDIS:ErrorMessage(LocalPlayer(), "Common.NotInTARDIS")
-            end
-        end
-    })
 
 end
 
