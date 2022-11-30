a96218840a7d376b6e8ecf4329825f9224b3f8c8
Artiom Parygin <artiom.par@ya.ru>
2022-11-30 09:45:03 +0300

    Simplified double teleport control to reference the original

--- lua/tardis/controls/sh_control_teleport_double.lua
+++ lua/tardis/controls/sh_control_teleport_double.lua
@@ -1,29 +1,11 @@
 TARDIS:AddControl({ id = "teleport_double",
     ext_func=function(self,ply,part)
-        if (self:GetData("teleport") or self:GetData("vortex")) and part:GetOn() then
-            self:Mat(function(result)
-                if result then
-                    TARDIS:Message(ply, "Controls.Teleport.Mat")
-                else
-                    TARDIS:ErrorMessage(ply, "Controls.Teleport.FailedMat")
-                end
-            end)
-        elseif not part:GetOn() then
-            local pos = pos or self:GetData("demat-pos") or self:GetPos()
-            local ang = ang or self:GetData("demat-ang") or self:GetAngles()
-            self:Demat(pos, ang, function(result)
-                if result then
-                    TARDIS:Message(ply, "Controls.Teleport.Demat")
-                else
-                    if self:GetData("doorstatereal", false) then
-                        TARDIS:ErrorMessage(ply, "Controls.Teleport.FailedDematDoorsOpen")
-                    elseif self:GetData("handbrake", false) then
-                        TARDIS:ErrorMessage(ply, "Controls.Teleport.FailedDematHandbrake")
-                    else
-                        TARDIS:ErrorMessage(ply, "Controls.Teleport.FailedDemat")
-                    end
-                end
-            end)
+        local on = part:GetOn()
+        local tp = self:GetData("teleport")
+        local vx = self:GetData("vortex")
+
+        if (tp and on) or (vx and on) or (not on and not tp and not vx)
+            TARDIS:Control("teleport", ply, part)
         end
     end,
     serveronly=true,
