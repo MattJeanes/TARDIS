f6c1337033f4752d2173d445231e40edd02cc721
Artiom Parygin <artiom.par@ya.ru>
2023-01-20 14:33:04 +0300

    Ensure UI closing when the entity is removed

--- lua/tardis/sh_screen_ui.lua
+++ lua/tardis/sh_screen_ui.lua
@@ -276,6 +276,13 @@ function TARDIS:HUDScreen(window)
     screen.gap2=screen.gap*2
     screen.ext=TARDIS:GetExteriorEnt()
     screen.int=TARDIS:GetInteriorEnt()
+
+    screen.Think = function(self)
+        if not IsValid(screen.ext) then
+            TARDIS:RemoveHUDScreen()
+        end
+    end
+
     screen:SetSize(screen.width,screen.height)
     screen:SetPos(2,25)
     self:LoadScreenUI(screen)
