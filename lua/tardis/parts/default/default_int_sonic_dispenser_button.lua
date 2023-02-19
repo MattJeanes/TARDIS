f8f43f0ec598ec4a4d4ede890c7ecd6a631cfab3
Poogie <andreupsgusarov@gmail.com>
2023-02-19 18:54:12 +0300

    Sonic dispenser sound

--- lua/tardis/parts/default/default_int_sonic_dispenser_button.lua
+++ lua/tardis/parts/default/default_int_sonic_dispenser_button.lua
@@ -7,5 +7,6 @@ PART.AutoSetup = true
 PART.Collision = true
 PART.Animate = true
 PART.Model = "models/drmatt/tardis/smallbutton.mdl"
+PART.Sound = "p00gie/tardis/controls/button.wav"
 
 TARDIS:AddPart(PART)
