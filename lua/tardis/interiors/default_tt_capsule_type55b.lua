640924ed6563270e936a2ceeedd8b053813879dd
Matt Jeanes <mattjeanes23@gmail.com>
2023-02-22 20:19:17 +0000

    fixed portal thiccness on type 55b

--- lua/tardis/interiors/default_tt_capsule_type55b.lua
+++ lua/tardis/interiors/default_tt_capsule_type55b.lua
@@ -29,6 +29,11 @@ T.Interior = {
         },
     },
 }
+T.Exterior = {
+    Portal = {
+        thickness = 30
+    }
+}
 
 T.Templates = {
     ttcapsule = { override = true, fail = function() ErrorNoHalt("Failed to add ttcapsule_type55b default exterior") end, },
