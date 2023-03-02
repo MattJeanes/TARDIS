8450929f102c6d3a8961fec0176c413f8f976fb4
Artiom Parygin <artiom.par@ya.ru>
2023-03-02 12:03:29 +0300

    Parts think refactored

--- lua/tardis/sh_parts.lua
+++ lua/tardis/sh_parts.lua
@@ -53,7 +53,19 @@ local overrides={
         local int=self.interior
         local ext=self.exterior
         if self._init and IsValid(int) and IsValid(ext) then
-            if (int:CallHook("ShouldThink")~=false) or (ext:DoorOpen() and self.ClientThinkOverride and LocalPlayer():GetPos():Distance(ext:GetPos())<TARDIS:GetSetting("portals-closedist")) or self.ExteriorPart then -- TODO: Improve
+            local think_ok = (int:CallHook("ShouldThink") ~= false)
+
+            local function is_visible_through_door()
+                local door = ext:DoorOpen()
+                local ply_pos = LocalPlayer():GetPos()
+                local ext_pos = ext:GetPos()
+                local think_override = self.ClientThinkOverride
+                local close_dist = TARDIS:GetSetting("portals-closedist")
+
+                return (door and think_override and ply_pos:Distance(ext_pos) < close_dist)
+            end
+
+            if think_ok or self.ExteriorPart or is_visible_through_door() then
                 if self.Animate then
                     local target=self:GetOn() and 1 or 0
                     self.posepos=math.Approach(self.posepos,target,FrameTime()*(self.AnimateSpeed or 1.5))
