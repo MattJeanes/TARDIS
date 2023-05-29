4dd01eb30ca35291a2d39714f4e922af963b93a3
Matt Jeanes <mattjeanes23@gmail.com>
2023-05-29 21:02:35 +0100

    fix artron energy low chameleon circuit failed, fixes #910

--- lua/entities/gmod_tardis/modules/sh_artronenergy.lua
+++ lua/entities/gmod_tardis/modules/sh_artronenergy.lua
@@ -281,7 +281,7 @@ if SERVER then
         if not TARDIS:GetSetting("artron_energy") then return end
 
         if self:GetData("artron-val") + TARDIS.artron_values.cost_chameleon < 1 then
-            return false,false,"Chameleon.FailReasons.NotEnoughArtron",true
+            return false,true,"Chameleon.FailReasons.NotEnoughArtron",true
         end
     end)
 
