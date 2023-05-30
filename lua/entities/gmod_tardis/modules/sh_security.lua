20dc5a6746a47abe6791fa24abbf25478d8db1a4
Matt Jeanes <mattjeanes23@gmail.com>
2023-05-30 01:13:51 +0100

    Added admin toggle for security override

--- lua/entities/gmod_tardis/modules/sh_security.lua
+++ lua/entities/gmod_tardis/modules/sh_security.lua
@@ -5,7 +5,7 @@ function ENT:GetSecurity()
 end
 
 function ENT:CheckSecurity(ply)
-    return (not self:GetSecurity()) or (ply==self:GetCreator()) or ply:IsAdmin()
+    return (not self:GetSecurity()) or (ply==self:GetCreator()) or (TARDIS:GetSetting("admin_security_bypass") and ply:IsAdmin())
 end
 
 if SERVER then
