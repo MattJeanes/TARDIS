a971f91909004ccbe120a7756d4be282b2573d41
Ryan Maybury <ryan2711@hotmail.co.uk>
2023-08-01 04:04:19 +0100

    Loading online players list

--- lua/tardis/screens/cl_screen_companions.lua
+++ lua/tardis/screens/cl_screen_companions.lua
@@ -1,3 +1,6 @@
+local isValid = IsValid
+local localPlayer = LocalPlayer
+
 TARDIS:AddScreen("Companions", {id="companions", text="Screens.CompanionSystem", menu=false, order=2, popuponly=false}, function(self,ext,int,frame,screen)
 --------------------------------------------------------------------------------
 -- Layout calculations
@@ -6,18 +9,20 @@ TARDIS:AddScreen("Companions", {id="companions", text="Screens.CompanionSystem",
     local h = frame:GetTall()
 
     local gap = math.min(w, h) * 0.06
-    local gap2 = math.min(w, h) * 0.10
+    local gap2 = math.min(w, h) * 0.02
 
     local listW = w * 0.3
     local listH = h - 2 * gap
 
     local pnlW = w - (listW*2) - (gap*4)
-    local pnlH = listH * 0.5
+    local pnlH = listH * 0.3
 
     local list2X = gap + listW + gap
 
     local pnlX = list2X + listW + gap
-    local pnlY = gap + (listW / 2) * 0.6
+    local pnlY = gap + (listW / 2)
+
+    local font = TARDIS:GetScreenFont(screen, "Default")
 --------------------------------------------------------------------------------
 -- Layout
 --------------------------------------------------------------------------------
@@ -49,12 +54,49 @@ TARDIS:AddScreen("Companions", {id="companions", text="Screens.CompanionSystem",
     players_list:SetPos(list2X, gap)
     players_list:SetMultiSelect(false)
     players_list:AddColumn("Online Players")
+    players_list.players = {}
+
+    players_list.AddPlayer = function(me, ply)
+        local name = ply:Name()
+        local steamid = ply:SteamID()
+        
+        me:AddLine(name)
+        me.players[#me.players + 1] = {name=name, steamid=steamid}
+    end
+
+    players_list.PlayerAlreadyExists = function(me, ply)
+        for _, v in ipairs(me.players) do
+            if v.steamid ~= ply:SteamID() then continue end
+            return true
+        end
+
+        return false
+    end
 
     local panel = frame:Add("DPanel")
     panel:SetSize(pnlW, pnlH)
     panel:SetPos(pnlX, pnlY)
     panel:SetBackgroundColor(bgcolor)
+
+    local add_player_btn = panel:Add("DButton")
+    add_player_btn:Dock(BOTTOM)
+    add_player_btn:DockMargin(gap2, gap2, gap2, gap2)
+    add_player_btn:SetTall(panel:GetWide() * 0.25)
+    add_player_btn:SetText("Add Player")
+    add_player_btn:SetFont(font)
+
+    local steamid_entry = panel:Add("DTextEntry3D2D")
+    steamid_entry.is3D2D = screen.is3D2D
+    steamid_entry:Dock(FILL)
+    steamid_entry:DockMargin(gap2, gap2, gap2, 0) -- Don't need more padding on bottom, add_player_btn is giving us this
+    steamid_entry:SetPlaceholderText("Enter a player steamid here")
+    steamid_entry:SetFont(font)
 --------------------------------------------------------------------------------
 -- Loading data
 --------------------------------------------------------------------------------
+    -- Add online players to list
+    for _, ply in ipairs(player.GetAll()) do
+        if not isValid(ply) or ply == localPlayer() or players_list:PlayerAlreadyExists(ply) then continue end
+        players_list:AddPlayer(ply)
+    end
 end)
