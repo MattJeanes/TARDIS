e46b5f78fb0d6e4d864aa76097d036f3d768469c
Artiom Parygin <artiom.par@ya.ru>
2023-10-26 18:08:52 +0300

    Revert "SlowThink hooks added"
    This reverts commit b5897e36f66db932585795731f2b52155f91f6b6.

--- lua/entities/gmod_tardis/init.lua
+++ lua/entities/gmod_tardis/init.lua
@@ -45,7 +45,6 @@ end
 function ENT:PhysicsCollide(colData, collider)
     self:CallHook("PhysicsCollide", colData, collider)
 end
-
 function ENT:OnTakeDamage(dmginfo)
     if self:CallHook("ShouldTakeDamage",dmginfo)==false then return end
     self:CallHook("OnTakeDamage", dmginfo)
@@ -57,13 +56,3 @@ duplicator.RegisterEntityClass("gmod_tardis", function(ply, data)
     ent:Initialize()
     return ent
 end, "Data")
-
-function ENT:Think()
-    self.BaseClass.Think(self)
-    if not self._init then return end
-
-    if CurTime() >= (self.nextslowthink or 0) then
-        self.nextslowthink = CurTime() + 1
-        self:CallHook("SlowThink")
-    end
-end
