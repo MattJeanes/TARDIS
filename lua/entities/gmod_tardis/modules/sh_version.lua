2216f116a8f8eff1304bfc6db84bc0b5bc5beeb5
Matt Jeanes <mattjeanes23@gmail.com>
2023-11-16 22:13:43 +0000

    add dynamic release notes popup

--- lua/entities/gmod_tardis/modules/sh_version.lua
+++ lua/entities/gmod_tardis/modules/sh_version.lua
@@ -10,9 +10,54 @@ ENT:AddHook("Initialize", "version", function(self)
     TARDIS:SetLastUsedVersion()
 end)
 
-if CLIENT then
-    ENT:AddHook("NewVersion", "version", function(self, newVersion, oldVersion)
-        -- TODO
-        TARDIS:Message(self:GetCreator(), "TARDIS updated from " .. TARDIS:GetVersionString(oldVersion) .. " to " .. TARDIS:GetVersionString(newVersion))
-    end)
+if SERVER then return end
+
+local function get_release_notes(newVersion)
+    local show = TARDIS:GetSetting("show_release_notes")
+    if not show then return end
+    local headers = {
+        ["X-GitHub-Api-Version"] = "2022-11-28"
+    }
+    local function onsuccess(body, size, headers, code)
+        if code ~= 200 then
+            print("Failed to retrieve TARDIS release notes for " .. newVersion .. ", code: " .. code .. ", body: " .. body)
+            return
+        end
+        local data = util.JSONToTable(body)
+        local release_notes = data.body
+        local json = string.match(release_notes, "<!%-%-(.-)%-%->")
+        if not json then return end
+        local releaseMetadata = util.JSONToTable(json)
+        if releaseMetadata.showPopup and releaseMetadata.summary then
+            local notes = releaseMetadata.summary
+            if releaseMetadata.changes then
+                notes = notes .. "\n"
+                for k,v in ipairs(releaseMetadata.changes) do
+                    notes = notes .. "\n" .. "• ".. v
+                end
+            end
+            Derma_Query(
+                TARDIS:GetPhrase("Update.Updated", newVersion) .. "\n\n" .. notes .. "\n\n" .. TARDIS:GetPhrase("Update.ShowReleaseNotes"),
+                "TARDIS Update",
+                TARDIS:GetPhrase("Common.Yes"),
+                function()
+                    gui.OpenURL(data.html_url)
+                end,
+                TARDIS:GetPhrase("Common.No"),
+                function() end,
+                TARDIS:GetPhrase("Common.DontAskAgain"),
+                function()
+                    TARDIS:SetSetting("show_release_notes", false)
+                end
+            )
+        end
+    end
+    local function onfailure(reason)
+        print("Failed to retrieve TARDIS release notes for " .. newVersion .. ": " .. reason)
+    end
+    http.Fetch("https://api.github.com/repos/MattJeanes/TARDIS/releases/tags/" .. newVersion, onsuccess, onfailure, headers)
 end
+
+ENT:AddHook("NewVersion", "version", function(self, newVersion, oldVersion)
+    get_release_notes(newVersion)
+end)
