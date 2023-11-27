95822a0d7e20a749fee575d99065cdad87ec3466
Matt Jeanes <mattjeanes23@gmail.com>
2023-11-27 21:13:44 +0000

    fix #1015

--- lua/tardis/interiors/default.lua
+++ lua/tardis/interiors/default.lua
@@ -131,6 +131,7 @@ T.Interior = {
             width = 378,
             height = 198,
             gui_rows = 4,
+            power_off_black = false,
         },
         {
             pos = Vector(33.658, 35.45, 159.97),
@@ -138,6 +139,7 @@ T.Interior = {
             width = 378,
             height = 198,
             gui_rows = 4,
+            power_off_black = false,
         },
         {
             pos = Vector(-27.484, -23.735, 165.416),
@@ -145,6 +147,7 @@ T.Interior = {
             width = 378,
             height = 198,
             gui_rows = 4,
+            power_off_black = false,
         },
         {
             pos = Vector(27.484, 23.735, 165.416),
@@ -152,6 +155,7 @@ T.Interior = {
             width = 378,
             height = 198,
             gui_rows = 4,
+            power_off_black = false,
         },
     },
 
