47b8fa8fcdc2122d6cf9eba5a03ce8054306ac77
MattJeanes <MattJeanes@users.noreply.github.com>
2024-01-18 06:23:13 +0000

    Update Generated Files

---
+++ lua/tardis/languages/el.lua
@@ -0,0 +1,14 @@
+-- AUTO GENERATED FILE - DO NOT EDIT --
+-- SOURCE FILE: i18n/languages/el.json --
+
+local T = {}
+T.Code = "el"
+T.Name = "Αγγλικά"
+T.Phrases = {
+    ["Artron.Depleted"] = "Η Άρτρον ενέργεια εξαντλήθηκε",
+    ["ArtronInhibitor"] = "Αναστολέας Άρτρον",
+    ["ArtronInhibitor.Disabled"] = "Ο αναστολέας Άρτρον απενεργοποιήθηκε",
+    ["ArtronInhibitor.Enabled"] = "Ο αναστολέας Άρτρον ενεργοποιήθηκε",
+}
+
+TARDIS:AddLanguage(T)
